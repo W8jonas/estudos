0742238cb79f4450a605997a4da48e626031f84f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-07-30 20:09:11 -0300

    Componente ButtonLink e Menu

---
+++ Alura/imersaoReact/aluraflix/src/components/ButtonLink/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ButtonLink({ className, href, children }) {
+  return (
+    <a className={className} href={href}>
+      {children}
+    </a>
+  );
+}
+
+ButtonLink.defaultProps = {
+  href: '/',
+  className: '',
+};
+
+ButtonLink.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+export default ButtonLink;
--- Alura/imersaoReact/aluraflix/src/components/Menu/index.js
+++ Alura/imersaoReact/aluraflix/src/components/Menu/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Logo from '../../assets/Logo.png'
+import ButtonLink from '../ButtonLink/index'
 
-// import { Container } from './styles';
+import './Menu.css'
 
-const Menu = () => {
+
+function Menu(){
   return (
-    <header>
-        Aluraflix
-    </header>
+    <nav className="Menu">
+        <a href='/'>
+            <img className="Logo" src={Logo} alt="AluraFlix" />
+        </a>
+
+        <ButtonLink className="ButtonLink" href='/'>
+            Novo vídeo
+        </ButtonLink>
+    </nav>
   )
 }
 
