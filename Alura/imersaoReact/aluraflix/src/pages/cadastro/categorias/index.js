17dad634e74fa4da988e2c4b12a031cad2de432d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-01 17:24:19 -0300

    Formulario de cadastro de categorias

--- Alura/imersaoReact/aluraflix/src/pages/cadastro/categorias/index.js
+++ Alura/imersaoReact/aluraflix/src/pages/cadastro/categorias/index.js
@@ -1,27 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/pageDefault/index'
 import {Link} from 'react-router-dom'
 
 
 function CadastroCategoria() {
+
+  const initialCategoryState = {
+    name: 'Filme',
+    desc: 'Descrição inicial',
+    color: '#ccdddd',
+  }
+
+  const [category, setCategory] = useState(initialCategoryState)
+  const [categories, setCategories] = useState([initialCategoryState])
+
+
+  function handleSubmit(event) {
+      event.preventDefault()
+      setCategories([...categories, category])
+      setCategory(initialCategoryState)
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {category.name}</h1>
 
-      <form>
-        <label> 
-          Nome da Categoria:
+      <form onSubmit={handleSubmit}>
 
-          <input 
-            type='text'
-          />
-        </label>
+        <div>
+          <label> 
+            Nome da Categoria: 
+            <input 
+              type='text'
+              value={category.name}
+              onChange={(e)=>setCategory({...category, name: e.target.value})}
+            />
+          </label>
+
+          <label> 
+            descrição: 
+            <textarea 
+              type='text'
+              value={category.desc}
+              onChange={(e)=>setCategory({...category, desc: e.target.value})}
+            />
+          </label>
+
+          <label> 
+            Cor: 
+            <input 
+              type='color'
+              value={category.color}
+              onChange={(e)=>setCategory({...category, color: e.target.value})}
+            />
+          </label>
+
+        </div>
 
         <button>
           Cadastrar
         </button>
 
       </form>
+
+      <ul>
+        {categories.map((category, index)=>(
+          <li key={index}>
+            {category.name}
+          </li>
+        ))}
+      </ul>
 
       <Link to="/">
         Cadastrar Categoria
