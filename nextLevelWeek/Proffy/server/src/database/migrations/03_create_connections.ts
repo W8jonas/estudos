b732009f72ca1f3e8b2d1361944b7e109678c338
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-10 00:45:15 -0300

    criando banco de dados usando knex e configurando as migrations

---
+++ nextLevelWeek/Proffy/server/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,23 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('connections', table => {
+        table.increments('id').primary()
+
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+
+        table.timestamp('create_at')
+            .defaultTo('now()')
+            .notNullable()
+    })
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('connections')
+}
+
