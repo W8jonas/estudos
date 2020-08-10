3973d6b440954a3d22299b03a039110a3741626f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-10 20:27:16 -0300

    ErroFix - query classControllers index

--- nextLevelWeek/Proffy/server/src/controllers/ClassesControllers.ts
+++ nextLevelWeek/Proffy/server/src/controllers/ClassesControllers.ts
@@ -28,15 +28,15 @@ export default class ClassesControllers {
         const timeInMinutes = convertHoursToMinutes(filters.time as string)
         const classes = await db('classes')
             .whereExists(function(){
-                this.select('classes_schedule.*')
+                this.select('class_schedule.*')
                     .from('class_schedule')
-                    .whereRaw('`class_schedule`.`class_id` = `classes`. `id`')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
                     .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
                     .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
                     .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
             })
             .where('classes.subject', '=', subject)
-            .join('users', 'classes.user_id', '=', 'user_id.')
+            .join('users', 'classes.user_id', '=', 'users.id')
             .select(['classes.*', 'users.*'])
 
         return response.json(classes)
