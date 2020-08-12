bf17697b44a0f5ab4a7f60f42e3f17a7eb31dbd1
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-12 20:07:16 -0300

    Adicionando comp de Select

--- nextLevelWeek/Proffy/web/src/pages/TeacherList/index.tsx
+++ nextLevelWeek/Proffy/web/src/pages/TeacherList/index.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 // assets
 import './styles.css'
 
-// Functions
 
 // Components
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
 import Input from '../../components/Input'
 import Select from '../../components/Select'
+
+// Functions
+import {subjectOptions, weekDayOptions} from '../../extras/index'
 
 
 function TeacherList() {
@@ -21,23 +23,13 @@ function TeacherList() {
                     <Select
                         name="subject"
                         label="Matéria"
-                        options={[
-                            {value: "Português", label: "Português"},
-                            {value: "Matemática", label: "Matemática"},
-                            {value: "Física", label: "Física"},
-                            {value: "Química", label: "Química"},
-                            {value: "Biologia", label: "Biologia"},
-                            {value: "História", label: "História"},
-                            {value: "Literatura", label: "Literatura"},
-                            {value: "Inglês", label: "Inglês"},
-                            {value: "Artes", label: "Artes"},
-                            {value: "Filosofia", label: "Filosofia"},
-                            {value: "Sociologia", label: "Sociologia"},
-                            {value: "Geografia", label: "Geografia"},
-                            {value: "Educação Física", label: "Educação Física"},
-                        ]}
+                        options={subjectOptions}
                     />
-                    <Input name="week_day" label="Dia da semana" />
+                    <Select
+                        name="week_day"
+                        label="Dia da semana"
+                        options={weekDayOptions}
+                    />
                     <Input type="time" name="time" label="Hora" />
                 </form>
 
