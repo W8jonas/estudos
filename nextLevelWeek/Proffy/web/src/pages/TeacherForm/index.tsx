d031c515fdf84c4d1399f11bd5a282be145d631a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-11 22:48:45 -0300

    atualizando para novo componente textarea

--- nextLevelWeek/Proffy/web/src/pages/TeacherForm/index.tsx
+++ nextLevelWeek/Proffy/web/src/pages/TeacherForm/index.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import Input from '../../components/Input'
 
 import WarningImage from '../../assets/icons/warning.svg'
+import Textarea from '../../components/Textarea'
 
 function TeacherForm() {
     return (
@@ -20,7 +21,7 @@ function TeacherForm() {
                     <Input name="name" label="Nome completo"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="Whatsapp" label="Whatsapp"/>
-
+                    <Textarea name="bio" label="Biografia"/>
                 </fieldset>
 
                 <fieldset>
