01ae239d7c1fcbc96d02312f2fe736f61f6e6b8d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-11 22:32:30 -0300

    Criando pagina TeacherForm

--- nextLevelWeek/Proffy/web/src/pages/TeacherForm/index.tsx
+++ nextLevelWeek/Proffy/web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
 
+import './styles.css'
+import Input from '../../components/Input'
+
+import WarningImage from '../../assets/icons/warning.svg'
+
 function TeacherForm() {
     return (
-        <PageHeader title="Incrível que você quer dar aulas"/>
+        <div id="page-teacher-form" className="container">
+            <PageHeader 
+                title="Incrível que você quer dar aulas"
+                description="O primeiro passo é preencher esse formulário de inscrição."
+            />
+
+            <main>
+                <fieldset>
+                    <legend> Seus dados </legend>
+                    <Input name="name" label="Nome completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="Whatsapp" label="Whatsapp"/>
+
+                </fieldset>
+
+                <fieldset>
+                    <legend> Sobre a aulas </legend>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora por aula"/>
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={WarningImage} alt="Aviso importante"/>
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+
+                    <button type="submit">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
+        </div>
     )
 }
 
