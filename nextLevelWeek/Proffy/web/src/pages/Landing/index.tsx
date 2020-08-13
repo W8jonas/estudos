410c8dee2990445286621d028e43ba5fa43a47e9
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-13 12:30:07 -0300

    Mostrando total de  coneções

--- nextLevelWeek/Proffy/web/src/pages/Landing/index.tsx
+++ nextLevelWeek/Proffy/web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { Link } from "react-router-dom";
 
@@ -12,8 +12,20 @@ import studyIcon from '../../assets/icons/study.svg'
 import giveClassesIcon from '../../assets/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/icons/purple-heart.svg'
 
+import api from '../../services/api'
 
 function Landing() {
+
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(()=>{
+        api.get('connections').then(response => {
+            const {total} = response.data
+            setTotalConnections(total)
+        })
+    }, [])
+
+    
   return (
     <div id="page-landing">
         <div id="page-landing-content" className="container">
@@ -36,7 +48,7 @@ function Landing() {
                 </Link>
             </div>
             <span className="total-connections">
-                Total de 200 conexões ja realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                Total de {totalConnections} conexões ja realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
             </span>
         </div>
     </div>
