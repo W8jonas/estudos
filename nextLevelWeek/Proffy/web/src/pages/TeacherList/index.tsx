f3e2b165cd4a9487bb46a52e1445fe92c123259e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-13 12:33:49 -0300

    Adicionando estados na listagem dos professores

--- nextLevelWeek/Proffy/web/src/pages/TeacherList/index.tsx
+++ nextLevelWeek/Proffy/web/src/pages/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 // assets
 import './styles.css'
@@ -6,40 +6,73 @@ import './styles.css'
 
 // Components
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, {Teacher} from '../../components/TeacherItem'
 import Input from '../../components/Input'
 import Select from '../../components/Select'
 
 // Functions
 import {subjectOptions, weekDayOptions} from '../../extras/index'
+import api from '../../services/api'
 
 
 function TeacherList() {
+
+    const [subject, setSubject] = useState('')
+    const [week_day, setWeek_day] = useState('')
+    const [time, setTime] = useState('')
+
+    const [teachers, setTeachers] = useState([])
+
+    async function handleSubmit(event: FormEvent){
+        event.preventDefault()
+        const response = await api.get('classes', {
+            params: {
+                subject, 
+                week_day,
+                time
+            }
+        })
+        setTeachers(response.data)
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
                 
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={handleSubmit}>
                     <Select
                         name="subject"
                         label="Matéria"
                         options={subjectOptions}
+                        value={subject}
+                        onChange={(e)=> setSubject(e.target.value)}
                     />
                     <Select
                         name="week_day"
                         label="Dia da semana"
                         options={weekDayOptions}
+                        value={week_day}
+                        onChange={(e)=> setWeek_day(e.target.value)}
                     />
-                    <Input type="time" name="time" label="Hora" />
+                    <Input
+                        type="time"
+                        name="time"
+                        label="Hora"
+                        value={time}
+                        onChange={(e)=> setTime(e.target.value)}
+                    />
+
+                    <button type="submit">
+                        Buscar
+                    </button>
                 </form>
 
             </PageHeader>
 
             <main>
-                <TeacherItem/>
-                <TeacherItem/>
-                <TeacherItem/>
-                <TeacherItem/>
+                {teachers.map((teacher:Teacher)=>(
+                    <TeacherItem key={teacher.id} teacher={teacher}/>
+                ))}
             </main>
 
         </div>
