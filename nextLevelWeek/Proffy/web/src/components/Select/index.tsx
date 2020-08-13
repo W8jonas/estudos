d19cdbc8becd512f198a4c7ea53caeac61adb59f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-13 12:25:22 -0300

    alteração select

--- nextLevelWeek/Proffy/web/src/components/Select/index.tsx
+++ nextLevelWeek/Proffy/web/src/components/Select/index.tsx
@@ -15,7 +15,7 @@ const Select: React.FC<SelectProps> = ({name, label, options, ...rest}) => {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select defaultValue="" id={name} {...rest}>
+            <select value="" id={name} {...rest}>
                 <option value="" hidden>Selecione uma opção</option>
 
                 {options.map((option)=> 
