d726b18781fd3815686b64341a9dc89872998c79
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-13 12:32:15 -0300

    Criando nova coneção e mostrando os dados do professor

--- nextLevelWeek/Proffy/web/src/components/TeacherItem/index.tsx
+++ nextLevelWeek/Proffy/web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,58 @@ import React from 'react'
 // assets
 import './styles.css'
 import WhatsappIcon from '../../assets/icons/whatsapp.svg'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    id: string;
+    bio:string;
+    subject:string;
+    cost: number;
+    name:string;
+    avatar:string;
+    whatsapp:string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) =>{
+    
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teachers-item">
             <header>
-                <img alt="Avatar" src="https://avatars0.githubusercontent.com/u/39463872?s=460&u=1f4c467aaa2280affdd5c119e1870e64cd08b13d&v=4" />
+                <img alt={teacher.name} src={teacher.avatar} />
                 <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Diego Fernandes</span>
+                    <strong>{teacher.subject}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Um texto realmente qualquer por enquanto
-                <br/> <br/>
-                Um texto realmente qualquer por enquanto
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R%80,00</strong>
+                    <strong>{teacher.cost}</strong>
                 </p>
-                <button type="button">
+                
+                <a
+                    target="_blank"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={WhatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
