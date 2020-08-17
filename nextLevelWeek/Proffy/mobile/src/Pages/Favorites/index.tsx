0d4228dba9da5db2f52df9d939f8b5110c33fa15
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-17 20:58:54 -0300

    Renderizando lista de favoritos

--- nextLevelWeek/Proffy/mobile/src/Pages/Favorites/index.tsx
+++ nextLevelWeek/Proffy/mobile/src/Pages/Favorites/index.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // assets
 import styles from './styles'
 
 // components
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 
 // functions
-
+import AsyncStorage from '@react-native-community/async-storage'
+import { useFocusEffect } from '@react-navigation/native'
 
 import {View, Image, ImageBackground,  Text, ScrollView} from 'react-native'
 
 
 function Favorites() {
+    const [favorites, setFavorites] = useState([])
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then((response)=>{
+            if (response) {
+                const favoritedTeachers = JSON.parse(response)
+                setFavorites(favoritedTeachers)
+            }
+        })
+    }
+    
+    useFocusEffect(()=>{
+        loadFavorites()
+    })
 
     return (
         <View style={styles.container}>
@@ -26,10 +41,13 @@ function Favorites() {
                 style={styles.teacherList}
                 contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 16}}
             >
-                <TeacherItem/>
-                <TeacherItem/>
-                <TeacherItem/>
-                <TeacherItem/>
+                {favorites.map((teacher: Teacher)=> (
+                    <TeacherItem 
+                        key={teacher.id} 
+                        teacher={teacher}
+                        favorited={true}
+                    />
+                ))}
             </ScrollView>
 
         </View>
