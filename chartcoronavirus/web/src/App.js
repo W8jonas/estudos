b40f900b8927d4e3deba587a1b787eb22dd8a74d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-26 02:47:12 -0300

    Grafico 3d Removendo assets folder e iniciando grafico em 3d com echarts-gl

--- chartcoronavirus/web/src/App.js
+++ chartcoronavirus/web/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
+      
+import 'echarts-gl'
+import ReactEcharts from "echarts-for-react";
 
 function App() {
+  
+  const GL_OPTION = {
+
+    grid3D: {},
+    xAxis3D: {},
+    yAxis3D: {},
+    zAxis3D: {},
+    series: [{
+        type: 'scatter3D',
+        symbolSize: 25,
+        data: [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
+        itemStyle: {
+            opacity: 1
+        }
+    }]
+
+  }
+
+
   return (
     <div className="App">
-      Hello world
+      <ReactEcharts
+        option={GL_OPTION}
+      />
     </div>
   );
 }
