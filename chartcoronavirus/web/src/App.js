5db48cba790683a0a9b4864fc7c39e2ad684c8b1
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-08-27 18:54:03 -0300

    Estrutura inicial

--- chartcoronavirus/web/src/App.js
+++ chartcoronavirus/web/src/App.js
@@ -1,32 +1,79 @@
-import React from 'react';
-import './App.css';
+import * as React from "react"
+import './App.css'
       
 import 'echarts-gl'
 import ReactEcharts from "echarts-for-react";
 
+
+const database = require("./assets/data-gl/population.json")
+
+const baseTexture = require('./assets/data-gl/asset/world_topo_bathy_200401.jpg')
+const heightTexture = baseTexture
+const environment = require('./assets/data-gl/asset/starfield.jpg')
+
+
 function App() {
   
+  const data = database.filter(function (dataItem) {
+		return dataItem[2] > 0;
+	}).map(function (dataItem) {
+		return [dataItem[0], dataItem[1], Math.sqrt(dataItem[2])];
+	})
+  
   const GL_OPTION = {
-
-    grid3D: {},
-    xAxis3D: {},
-    yAxis3D: {},
-    zAxis3D: {},
-    series: [{
-        type: 'scatter3D',
-        symbolSize: 25,
-        data: [[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
-        itemStyle: {
-            opacity: 1
-        }
-    }]
-
+	backgroundColor: '#000',
+	globe: {
+		baseTexture: baseTexture,
+		heightTexture: heightTexture,
+		shading: 'lambert',
+		environment: environment,
+		light: {
+			main: {
+				intensity: 3
+			}
+		},
+		viewControl: {
+			autoRotate: false
+		}
+	},
+	visualMap: {
+		max: 40,
+		calculable: true,
+		realtime: false,
+		inRange: {
+			colorLightness: [0.2, 0.9]
+		},
+		textStyle: {
+			color: '#fff'
+		},
+		controller: {
+			inRange: {
+				color: '#284'
+			}
+		},
+		outOfRange: {
+			colorAlpha: 0
+		}
+	},
+	series: [{
+		type: 'bar3D',
+		coordinateSystem: 'globe',
+		data: data,
+		barSize: 0.6,
+		minHeight: 0.2,
+		silent: true,
+		itemStyle: {
+			color: '#284'
+		}
+	}]
+  
   }
 
 
   return (
     <div className="App">
       <ReactEcharts
+	  	style={{height: '500px', width: '100%'}}
         option={GL_OPTION}
       />
     </div>
