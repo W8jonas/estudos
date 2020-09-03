2dfd07e990b2598be469a1ba68b642667ef1e1e7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-03 15:05:01 -0300

    alterando cor do botao

--- Alura/imersaoReact/aluraflix/src/components/Button/index.js
+++ Alura/imersaoReact/aluraflix/src/components/Button/index.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-    color: var(--white);
-    border: 1px solid var(--white);
+    color: var(--primary);
+    border: 1px solid var(--primary);
     box-sizing: border-box;
     cursor: pointer;
     padding: 16px 24px;
