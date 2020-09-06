0d57173fe01ef56355d5f4fb1402c0bea6e318ef
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-05 22:34:22 -0300

    refatorando codigo para ler as partes do DB de forma otimizada

--- chartcoronavirus/web/src/utils/getAllData.js
+++ chartcoronavirus/web/src/utils/getAllData.js
@@ -6,56 +6,42 @@ import realData3 from '../assets/data-gl/separatedData/parte_3.json'
 
 export default function getAllData() {
     
+    function getPartialData(partialData) {
+        
+        let valueMaxFoundedInPartialData = 0
+
+        const partialDataProcessed = partialData.map((dataItem) => {
+            const totalConfirmed = Number(dataItem["totalConfirmed"])
+            if(totalConfirmed > valueMaxFoundedInPartialData) {
+                valueMaxFoundedInPartialData = totalConfirmed
+            }
+    
+            return [
+                dataItem["long"],
+                dataItem["lat"],
+                Math.sqrt(totalConfirmed)
+            ]
+        })
+        
+        return [partialDataProcessed, valueMaxFoundedInPartialData]
+    }
+
+    const partialDataArray = [realData1, realData2, realData3]
+    
     let valueMaxFounded = 0
+    let allData = []
 
-    const data1 = realData1
-    .map((dataItem) => {
-        
-        const totalConfirmed = Number(dataItem["totalConfirmed"])
-        if(totalConfirmed > valueMaxFounded) {
-            valueMaxFounded = totalConfirmed
+    partialDataArray.forEach((partialData)=>{
+        const [data, valueMaxFoundedInPartialData] = getPartialData(partialData)
+
+        allData = [...allData, ...data]
+        if(valueMaxFoundedInPartialData > valueMaxFounded) {
+            valueMaxFounded = valueMaxFoundedInPartialData
         }
-
-        return [
-            dataItem["long"],
-            dataItem["lat"],
-            Math.sqrt(totalConfirmed)
-        ]
     })
 
-    const data2 = realData2
-    .map((dataItem) => {
-        
-        const totalConfirmed = Number(dataItem["totalConfirmed"])
-        if(totalConfirmed > valueMaxFounded) {
-            valueMaxFounded = totalConfirmed
-        }
-
-        return [
-            dataItem["long"],
-            dataItem["lat"],
-            Math.sqrt(totalConfirmed)
-        ]
-    })
-
-    const data3 = realData3
-    .map((dataItem) => {
-        
-        const totalConfirmed = Number(dataItem["totalConfirmed"])
-        if(totalConfirmed > valueMaxFounded) {
-            valueMaxFounded = totalConfirmed
-        }
-
-        return [
-            dataItem["long"],
-            dataItem["lat"],
-            Math.sqrt(totalConfirmed)
-        ]
-    })
-
-
-    const allData = [...data1, ...data2, ...data3]
 
     return [allData, valueMaxFounded]
 }
+
 
