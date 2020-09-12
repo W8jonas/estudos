f2ade9a04cdab803220a80a57ac700f7858b1fdd
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-12 12:56:59 -0300

    atualizando codigo do server

--- Eletronics, ESP, Arduino and IOT/ComunicationWithSocket_ESP32_to_ESP8266/server_esp32/server_esp32.ino
+++ Eletronics, ESP, Arduino and IOT/ComunicationWithSocket_ESP32_to_ESP8266/server_esp32/server_esp32.ino
@@ -1,61 +1,87 @@
-//Apenas vai compilar o código contido neste arquivo
-//caso IS_SERVER esteja definido
 
 #include <WiFi.h>
+#include ".\env.h"
 
 
-//Cria o server na porta definida por 'SERVER_PORT'
+IPAddress local_IP(ENV_LOCAL_IP[0], ENV_LOCAL_IP[1], ENV_LOCAL_IP[2], ENV_LOCAL_IP[3]);
+IPAddress gateway(ENV_GATEWAY[0], ENV_GATEWAY[1], ENV_GATEWAY[2], ENV_GATEWAY[3]);
+IPAddress subnet(ENV_SUBNET[0], ENV_SUBNET[1], ENV_SUBNET[2], ENV_SUBNET[3]);
+
 WiFiServer server(ENV_SERVER_PORT);
 
+
 enum Protocol{
-    PIN, //Pino que se deseja alterar o estado
-    VALUE, //Estado para qual o pino deve ir (HIGH = 1 ou LOW = 0)
-    BUFFER_SIZE //O tamanho do nosso protocolo. IMPORTANTE: deixar sempre como último do enum
+    PIN,
+    VALUE,
+    BUFFER_SIZE
 };
 
+
+#define indicative_led 13
 
 void setup()
 {
     Serial.begin(115200);
-  
-    //Coloca este ESP como Access Point
+    
+    pinMode(indicative_led, OUTPUT);
+
     WiFi.mode(WIFI_AP);
-    //SSID e Senha para se conectarem a este ESP
-    WiFi.softAP(ENV_SSID, ENV_PASSWORD);
-    //Inicia o server
+
+    while (!(WiFi.softAP(ENV_SSID, ENV_PASSWORD, 6, false))) {
+        Serial.println(".");  
+        delay(100);
+    }
+
+    while (!(WiFi.softAPConfig(local_IP, gateway, subnet))) {
+        Serial.println(".");  
+        delay(100);
+    }
+
     server.begin();
-    Serial.print("Wait for WiFi");
+    server.setNoDelay(true);
+
 }
+
+
 void loop()
 {
+
+    while (WiFi.softAPgetStationNum() == 0)
+    {
+        //aguarda 1 segundo
+        delay(1000);
+
+        //se alguém conectou
+        if (WiFi.softAPgetStationNum() != 0)
+        {
+            //exibe mensagem
+            Serial.println("STA conectada!");
+        }
+    }
+
     //Verifica se tem algum cliente se conectando
     WiFiClient client = server.available();
-    if (client) 
-    {      
-        Serial.println("Achou client");
-        //Se o cliente tem dados que deseja nos enviar
-        if (client.available())
-        {
-            //Criamos um buffer para colocar os dados 
-            uint8_t buffer[Protocol::BUFFER_SIZE];
-            //Colocamos os dados enviados pelo cliente no buffer
-            int len = client.read(buffer, Protocol::BUFFER_SIZE);
-            //Verificamos qual o pino que o cliente enviou
-            int pinNumber = buffer[Protocol::PIN];
-            //Verificamos qual o valor deste pino
-            int value = buffer[Protocol::VALUE];
-            //Colocamos o pino em modo de saída
-            pinMode(pinNumber, OUTPUT);
-            // Alteramos o estado do pino para o valor passado
-            digitalWrite(pinNumber, value);
-            Serial.print("pegou dado ");
-            Serial.print(pinNumber);
-            Serial.print(" -- ");
-            Serial.println(value);
-            Serial.println("_______________");
-        }
 
-        //Fecha a conexão com o cliente
+    if (client)
+    {
+        digitalWrite(indicative_led, HIGH);
+
+        while (client.connected())
+        {
+            if (client.available())
+            { 
+                uint8_t buffer[Protocol::BUFFER_SIZE];
+
+                int len = client.read(buffer, Protocol::BUFFER_SIZE);
+                int pinNumber = buffer[Protocol::PIN];
+                int value = buffer[Protocol::VALUE];
+                
+                pinMode(pinNumber, OUTPUT);
+                digitalWrite(pinNumber, value);
+            }
+        }
+        delay(1); 
         client.stop();
+        digitalWrite(indicative_led, LOW);
     }
 }
