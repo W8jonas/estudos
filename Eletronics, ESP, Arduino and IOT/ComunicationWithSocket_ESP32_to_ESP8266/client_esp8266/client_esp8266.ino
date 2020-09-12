0f62300dae3adf0e934f7a80cba4812dfe9bf09a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-12 00:00:29 -0300

    Iniciando com codigo do client

--- Eletronics, ESP, Arduino and IOT/ComunicationWithSocket_ESP32_to_ESP8266/client_esp8266/client_esp8266.ino
+++ Eletronics, ESP, Arduino and IOT/ComunicationWithSocket_ESP32_to_ESP8266/client_esp8266/client_esp8266.ino
@@ -1,9 +1,104 @@
+/*
+    This sketch sends a string to a TCP server, and prints a one-line response.
+    You must run a TCP server in your local network.
+    For example, on Linux you can use this command: nc -v -l 3000
+*/
 
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+
+#define IN_PIN 23
+
+const char* ssid     = ENV_STASSID;
+const char* password = ENV_STAPSK;
+
+const char* host = ENV_HOST;
+const uint16_t port = ENV_PORT;
+
+ESP8266WiFiMulti WiFiMulti;
+
+enum Protocol{
+    PIN, //Pino que se deseja alterar o estado
+    VALUE, //Estado para qual o pino deve ir (HIGH = 1 ou LOW = 0)
+    BUFFER_SIZE //O tamanho do nosso protocolo. IMPORTANTE: deixar sempre como último do enum
+};
 
 void setup() {
+  Serial.begin(115200);
 
+  // We start by connecting to a WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(500);
 }
 
-void loop() {
 
+void loop() {
+  Serial.print("connecting to ");
+  Serial.print(host);
+  Serial.print(':');
+  Serial.println(port);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    Serial.println("wait 5 sec...");
+    delay(5000);
+    return;
+  }
+  
+    //Criamos um buffer para colocar os dados 
+    uint8_t buffer[Protocol::BUFFER_SIZE];
+    
+    //Fazemos a leitura do pino
+    int value = digitalRead(IN_PIN);
+    
+    //Colocamos no buffer o número do pino
+    //cujo estado queremos enviar
+    buffer[Protocol::PIN] = IN_PIN;
+    
+    //Colocamos no buffer o estado atual do pino
+    buffer[Protocol::VALUE] = value;
+    
+    //Enviamos e finalizamos a conexão
+    client.write(buffer, Protocol::BUFFER_SIZE);
+    client.flush();
+    client.stop();
+     
+  /*
+  // This will send the request to the server
+  client.println("hello from ESP8266");
+
+  //read back one line from server
+  Serial.println("receiving from remote server");
+  String line = client.readStringUntil('\r');
+  Serial.println(line);
+
+  Serial.println("closing connection");
+  client.stop();
+
+  Serial.println("wait 5 sec...");
+  delay(5000);
+  */
+
+  
 }
