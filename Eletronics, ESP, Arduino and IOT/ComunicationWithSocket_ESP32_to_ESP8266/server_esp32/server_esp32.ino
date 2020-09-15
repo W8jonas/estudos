55682ea39f57f510b2debb8976668f7c39b03680
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-15 13:13:22 -0300

    Criando parte para saida PWM do sinal recebido

--- Eletronics, ESP, Arduino and IOT/ComunicationWithSocket_ESP32_to_ESP8266/server_esp32/server_esp32.ino
+++ Eletronics, ESP, Arduino and IOT/ComunicationWithSocket_ESP32_to_ESP8266/server_esp32/server_esp32.ino
@@ -17,13 +17,37 @@ enum Protocol{
 };
 
 
-#define indicative_led 13
+// use first channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0     0
+
+// use 13 bit precission for LEDC timer
+#define LEDC_TIMER_13_BIT  13
+
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ     5000
+
+#define indicative_led 23
+
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+  // calculate duty, 8191 from 2 ^ 13 - 1
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+
+  // write duty to LEDC
+  ledcWrite(channel, duty);
+}
+
 
 void setup()
 {
     Serial.begin(115200);
-    
-    pinMode(indicative_led, OUTPUT);
+
+    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+    ledcAttachPin(indicative_led, LEDC_CHANNEL_0);
+  
+    //pinMode(indicative_led, OUTPUT);
 
     WiFi.mode(WIFI_AP);
 
@@ -41,7 +65,6 @@ void setup()
 
     server.begin();
     server.setNoDelay(true);
-
 }
 
 
@@ -76,15 +99,16 @@ void loop()
 
                 int len = client.read(buffer, Protocol::BUFFER_SIZE);
                 int pinNumber = buffer[Protocol::PIN];
-                int value = buffer[Protocol::VALUE];
-
+                float value = buffer[Protocol::VALUE];
+                
                 Serial.print("Dados recebidos: ");
                 Serial.print(pinNumber);
                 Serial.print(": ");
                 Serial.println(value);
-                
-                pinMode(23, OUTPUT);
-                digitalWrite(23, value);
+
+                ledcAnalogWrite(LEDC_CHANNEL_0, value);
+                //pinMode(23, OUTPUT);
+                //digitalWrite(23, value);
             }
         }
         delay(1); 
