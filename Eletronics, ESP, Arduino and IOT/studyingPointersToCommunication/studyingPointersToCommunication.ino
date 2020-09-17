0225755fdf0d01814176f27b8b865befa2308393
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-17 19:23:39 -0300

    Estrutura inicial projeto

--- Eletronics, ESP, Arduino and IOT/studyingPointersToCommunication/studyingPointersToCommunication.ino
+++ Eletronics, ESP, Arduino and IOT/studyingPointersToCommunication/studyingPointersToCommunication.ino
@@ -1,11 +1,39 @@
 
+/*
+ * Projeto criado para estudar o comportamento de 
+ * ponteiros na familia ESP para comunicação de dados
+ * 
+*/
+
+
+typedef struct {
+  int counter;
+  float halfCounter;
+} Object;
+
+
+void setVariables(){
+  
+}
+
+
+void readVariables() {
+  
+}
+
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  
+  for(int i=0; i<=10; i++) Serial.println("");
+  
+  Serial.println("Initializing");
+  setVariables();
+  readVariables();
+  Serial.println("end");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
 }
