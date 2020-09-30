f86e80e5f50db9b22e6d979dc3abbf878f52cc45
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-09-30 01:12:09 -0300

    Limpando projeto inicial

---
+++ communication_with_esp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+const App = () => {
+  return (
+    <View>
+        <Text>
+        Hello App! 
+        </Text>
+    </View>
+  )
+}
+
+export default App;
