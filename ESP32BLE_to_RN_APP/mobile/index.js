34fa783efd18b8a269c1266f737c76e209228518
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-10-09 02:33:38 -0300

    trocando arquivo exemplo

--- ESP32BLE_to_RN_APP/mobile/index.js
+++ ESP32BLE_to_RN_APP/mobile/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/index';
+import App from './src/indexExemple';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
