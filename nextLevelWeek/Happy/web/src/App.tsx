4ae94c3b7c386d17a1fb44df5b3d8606a5299443
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-10-16 02:07:33 -0300

    criando navegação entre telas

--- nextLevelWeek/Happy/web/src/App.tsx
+++ nextLevelWeek/Happy/web/src/App.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
 
-import './styles/pages/landing.css'
 import './styles/global.css'
 
-import logoImg from './images/Logo.svg'
-import { FiArrowRight } from 'react-icons/fi'
+import Routes from './routes'
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-        
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
-
-        <div className="location">
-          <strong>Juiz de Fora</strong>
-          <span>Minas Gerais</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
-        </a>
-
-      </div>
-    </div>
+    <Routes/>
   );
 }
 
