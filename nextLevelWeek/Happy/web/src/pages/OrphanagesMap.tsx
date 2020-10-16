bf4fdee792b536ff2c253b7d309a34d1325e1bff
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-10-16 02:43:59 -0300

    estruturando e estilizando tela de orfanatos

--- nextLevelWeek/Happy/web/src/pages/OrphanagesMap.tsx
+++ nextLevelWeek/Happy/web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
 
+import '../styles/pages/OrphanagesMap.css'
 
-export default function Landing() {
+import logoImg from '../images/mapMarker.svg'
+import { FiPlus } from 'react-icons/fi'
+import {Link} from 'react-router-dom'
+
+
+export default function OrphanagesMap() {
   return(
-    <div id="page-landing">
-    
-			<h1>Mapa de orfanatos</h1>
-    
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={logoImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+
+          <p>Escolha um orfanato no mapa</p>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+      <footer>
+			  <strong>Juiz de Fora</strong>
+			  <span>Juiz de Fora</span>
+      </footer>
+
+      </aside>
+      
+      <div>
+
+      </div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF"/>
+      </Link>
+
     </div>
   )
 }
