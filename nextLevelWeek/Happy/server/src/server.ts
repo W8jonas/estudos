3e9813896976d31205411ee74f8030ac93f68ccf
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-10-18 01:25:28 -0300

    instalando typeorm e configurando db

--- nextLevelWeek/Happy/server/src/server.ts
+++ nextLevelWeek/Happy/server/src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import './database/connection'
 
 const app = express()
 
+app.use(express.json())
+
+
+app.get('/', (request, response) => {
+    return response.json({ok: "2OKK"})
+})
+
 app.listen(3333)
-
-
