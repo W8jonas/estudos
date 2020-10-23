8cca6f99fdd1d633198c735d857aad4979470ab0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-10-22 23:59:48 -0300

    adicionando index nas rotas

--- nextLevelWeek/Happy/server/src/routes.ts
+++ nextLevelWeek/Happy/server/src/routes.ts
@@ -3,6 +3,7 @@ import OrphanagesController from './controllers/OrphanagesController'
 
 const routes = Router()
 
+routes.get('/orphanages', OrphanagesController.index)
 routes.post('/orphanages', OrphanagesController.create)
 
 
