b4496bb56fa4e959c1bb02298381cc8551be7ecb
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-10-31 16:38:34 -0300

    criando rota de mostrar detalhes de um orfanato

--- nextLevelWeek/Happy/server/src/controllers/OrphanagesController.ts
+++ nextLevelWeek/Happy/server/src/controllers/OrphanagesController.ts
@@ -13,6 +13,16 @@ export default {
         return response.json(orphanages)
     },
 
+    async show(request: Request, response: Response) {
+        const { id } = request.params
+        
+        const orphanagesRepository = getRepository(Orphanage)
+        
+        const orphanage = await orphanagesRepository.findOneOrFail(id)
+        
+        return response.json(orphanage)
+    },
+
     async create(request: Request, response: Response) {
         const {
             name,
