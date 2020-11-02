a265c3b37a1591e9a421a4e99c0378d6b3f78b6e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-01 21:53:18 -0300

    repassando images no controller e add view method

--- nextLevelWeek/Happy/server/src/controllers/OrphanagesController.ts
+++ nextLevelWeek/Happy/server/src/controllers/OrphanagesController.ts
@@ -1,26 +1,31 @@
 
 import {getRepository} from 'typeorm'
-import Orphanage from '../models/Orphanages'
+import Orphanage from '../models/Orphanage'
 import { Request, Response } from 'express'
+import OrphanageView from '../views/orphanages_views'
 
 
 export default {
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage)
         
-        const orphanages = await orphanagesRepository.find()
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        })
         
-        return response.json(orphanages)
+        return response.json(OrphanageView.renderMany(orphanages))
     },
 
     async show(request: Request, response: Response) {
         const { id } = request.params
-        
+
         const orphanagesRepository = getRepository(Orphanage)
         
-        const orphanage = await orphanagesRepository.findOneOrFail(id)
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        })
         
-        return response.json(orphanage)
+        return response.json(OrphanageView.render(orphanage))
     },
 
     async create(request: Request, response: Response) {
@@ -36,6 +41,11 @@ export default {
     
         const orphanagesRepository = getRepository(Orphanage)
     
+        const requestImages = request.files as Express.Multer.File[]
+        const images = requestImages.map(image=>{
+            return {path: image.filename}
+        })
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -43,7 +53,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         })
     
         await orphanagesRepository.save(orphanage)
