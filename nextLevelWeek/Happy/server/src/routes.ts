2546f970c486c0f6deed3b22503e1defc37ce576
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-01 21:54:49 -0300

    adicionando rotas de images com multer

--- nextLevelWeek/Happy/server/src/routes.ts
+++ nextLevelWeek/Happy/server/src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 import OrphanagesController from './controllers/OrphanagesController'
+import multer from 'multer'
+
+import uploadConfig from './config/upload'
 
 const routes = Router()
+const upload = multer(uploadConfig)
 
 routes.get('/orphanages', OrphanagesController.index)
 routes.get('/orphanages/:id', OrphanagesController.show)
-routes.post('/orphanages', OrphanagesController.create)
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
 
 
 routes.get('/', (request, response) => {
