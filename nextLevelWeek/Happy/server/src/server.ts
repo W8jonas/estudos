cc43ed5653cc7e2fbbc362d6f5c2409a179c7ae4
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-02 23:03:06 -0300

    criando rota para pasta de uploads

--- nextLevelWeek/Happy/server/src/server.ts
+++ nextLevelWeek/Happy/server/src/server.ts
@@ -4,9 +4,12 @@ import './database/connection'
 
 import routes from './routes'
 
+import path from 'path'
+
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333)
