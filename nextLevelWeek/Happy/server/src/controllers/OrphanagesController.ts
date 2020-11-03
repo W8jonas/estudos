6743562d13862676879190d19833f23bc0c076d1
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-02 23:24:34 -0300

    adicionando Yup no controller

--- nextLevelWeek/Happy/server/src/controllers/OrphanagesController.ts
+++ nextLevelWeek/Happy/server/src/controllers/OrphanagesController.ts
@@ -3,6 +3,7 @@ import {getRepository} from 'typeorm'
 import Orphanage from '../models/Orphanage'
 import { Request, Response } from 'express'
 import OrphanageView from '../views/orphanages_views'
+import * as Yup from 'yup'
 
 
 export default {
@@ -46,7 +47,7 @@ export default {
             return {path: image.filename}
         })
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -55,7 +56,26 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required(),
+            }))
         })
+
+        await schema.validate(data, {
+            abortEarly: false,
+        })
+
+        const orphanage = orphanagesRepository.create(data)
     
         await orphanagesRepository.save(orphanage)
     
