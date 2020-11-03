58572f057b7377b0ebbf1bab59712bee44c3dabd
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-02 23:24:47 -0300

    adicionando Yup no handlerErrors

--- nextLevelWeek/Happy/server/src/errors/handler.ts
+++ nextLevelWeek/Happy/server/src/errors/handler.ts
@@ -1,9 +1,24 @@
 import { ErrorRequestHandler } from 'express'
+import { ValidationError } from 'yup'
 
+interface ValidationErrors {
+    [key: string]: string[]
+}
 
 const errorhandler: ErrorRequestHandler = (error, request, response, next) => {
     console.log(error);
     
+    if (error instanceof ValidationError) {
+        let yupErrors: ValidationErrors = {}
+
+        error.inner.forEach(err => {
+            yupErrors[err.path] = err.errors
+        })
+
+        return response.status(400).json({message: 'validation fails', errors: yupErrors})
+    
+    }
+
     return response.status(500).json({message: 'Interval server error'})
 }
 
