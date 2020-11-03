9cc9f12d74f128f425f4efc15e81c23b994eabb4
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-02 23:27:17 -0300

    instalando e add cors

--- nextLevelWeek/Happy/server/src/server.ts
+++ nextLevelWeek/Happy/server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import './database/connection'
 import path from 'path'
+import cors from 'cors'
 
 import routes from './routes'
 import errorHandler from './errors/handler'
@@ -9,6 +10,7 @@ import errorHandler from './errors/handler'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
