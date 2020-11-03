ab229960e8f5ffc6d8f8bc461783558ead042737
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-02 23:08:57 -0300

    adicionando rotas para handlerErrors

--- nextLevelWeek/Happy/server/src/server.ts
+++ nextLevelWeek/Happy/server/src/server.ts
@@ -1,16 +1,17 @@
 import express from 'express'
 import 'express-async-errors'
-
 import './database/connection'
+import path from 'path'
 
 import routes from './routes'
+import errorHandler from './errors/handler'
 
-import path from 'path'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333)
