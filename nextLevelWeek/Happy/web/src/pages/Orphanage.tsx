1b2dd2459682008ecad6deff2ea10390e9c47ab9
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-04 23:07:12 -0300

    integracao com backend

--- nextLevelWeek/Happy/web/src/pages/Orphanage.tsx
+++ nextLevelWeek/Happy/web/src/pages/Orphanage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
@@ -6,8 +6,41 @@ import { Map, Marker, TileLayer } from "react-leaflet";
 import '../styles/pages/orphanage.css';
 import Sidebar from "../components/Sidebar";
 import MapIcon from "../components/MapIcon";
+import api from "../services/api";
+import {useParams} from 'react-router-dom'
+
+interface Orphanages {
+  latitude: number,
+  longitude: number,
+  name: string,
+  about: string,
+  instructions: string,
+  opening_hours: string,
+  open_on_weekends: string,
+  images: Array <{
+    url: string,
+    id: number
+  }>
+}
+
+interface OrphanageParams {
+  id: string,
+}
 
 export default function Orphanage() {
+  const params = useParams<OrphanageParams>()
+  const [orphanage, setOrphanage] = useState<Orphanages>()
+  const [activeImageIndex, setActiveImageIndex] = useState(0)
+
+  useEffect(()=>{
+    api.get(`orphanages/${params.id}`).then((response)=>{
+      setOrphanage(response.data)
+    })
+  }, [params.id])
+
+  if(!orphanage) {
+    return <h3>Carregando...</h3>
+  }
 
   return (
     <div id="page-orphanage">
@@ -16,36 +49,28 @@ export default function Orphanage() {
       
       <main>
         <div className="orphanage-details">
-          <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
 
           <div className="images">
-            <button className="active" type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
+            {orphanage.images.map((image, index)=>(
+              <button 
+                type="button" 
+                key={image.id} 
+                className={activeImageIndex === index ? 'active' : ''} 
+                onClick={()=>setActiveImageIndex(index)}
+              >
+                <img src={image.url} alt={orphanage.name} />
+              </button>
+            ))}
           </div>
           
           <div className="orphanage-details-content">
-            <h1>Lar das meninas</h1>
-            <p>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</p>
+            <h1>{orphanage.name}</h1>
+            <p>{orphanage.about}</p>
 
             <div className="map-container">
               <Map 
-                center={[-27.2092052,-49.6401092]} 
+                center={[orphanage.latitude, orphanage.longitude]} 
                 zoom={16} 
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -57,36 +82,45 @@ export default function Orphanage() {
                 <TileLayer 
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
-                <Marker interactive={false} icon={MapIcon} position={[-27.2092052,-49.6401092]} />
+                <Marker interactive={false} icon={MapIcon} position={[orphanage.latitude, orphanage.longitude]} />
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude}, ${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
             <hr />
 
             <h2>Instruções para visita</h2>
-            <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+            <p>{orphanage.instructions}</p>
 
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15B6D6" />
-                Segunda à Sexta <br />
-                8h às 18h
+                {orphanage.opening_hours}
               </div>
-              <div className="open-on-weekends">
-                <FiInfo size={32} color="#39CC83" />
-                Atendemos <br />
-                fim de semana
-              </div>
+              {orphanage.open_on_weekends 
+              ? <div className="open-on-weekends">
+                  <FiInfo size={32} color="#39CC83" />
+                  Atendemos <br />
+                  fim de semana
+                </div>
+              : <div className="open-on-weekends dont-open">
+                  <FiInfo size={32} color="#FF6690" />
+                  Não atendemos <br />
+                  fim de semana
+                </div>
+              }
             </div>
 
-            <button type="button" className="contact-button">
+            {/* <button type="button" className="contact-button">
               <FaWhatsapp size={20} color="#FFF" />
               Entrar em contato
-            </button>
+            </button> */}
           </div>
         </div>
       </main>
