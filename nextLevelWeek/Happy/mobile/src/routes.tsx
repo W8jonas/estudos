ba3ce0008f0f20b3646d69c65c90e041869eb0ae
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-09 15:03:03 -0300

    adicionando comp Header na tela orphanageDetails

--- nextLevelWeek/Happy/mobile/src/routes.tsx
+++ nextLevelWeek/Happy/mobile/src/routes.tsx
@@ -12,13 +12,22 @@ import OrphanageDetails from './pages/OrphanageDetails/index'
 import OrphanageData from './pages/CreateOrphanage/OrphanageData/index'
 import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition/index'
 
+import Header from './components/Header/index'
 
 export default function Routes() {
     return (
         <NavigationContainer>
             <Navigator screenOptions={{ headerShown: false, cardStyle: {backgroundColor: '#f3f2f5'} }}>
                 <Screen name="OrphanagesMap" component={OrphanagesMap}/>
-                <Screen name="OrphanageDetails" component={OrphanageDetails}/>
+
+                <Screen 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails} 
+                    options={{
+                        headerShown: true, 
+                        header: () => <Header title="Orfanato"/>
+                    }}
+                />
 
                 <Screen name="OrphanageData" component={OrphanageData}/>
                 <Screen name="SelectMapPosition" component={SelectMapPosition}/>
