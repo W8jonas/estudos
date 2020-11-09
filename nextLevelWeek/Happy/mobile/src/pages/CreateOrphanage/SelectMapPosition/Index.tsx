a3d210c24a8e7cb0b95728d04b56d63c96a2b92f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-08 23:36:42 -0300

    corrigindo importacao da img

--- nextLevelWeek/Happy/mobile/src/pages/CreateOrphanage/SelectMapPosition/Index.tsx
+++ nextLevelWeek/Happy/mobile/src/pages/CreateOrphanage/SelectMapPosition/Index.tsx
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import MapView, { Marker } from 'react-native-maps';
 
-import mapMarkerImg from '../../images/map-marker.png';
+import mapMarkerImg from '../../../images/map-marker.png';
 
 export default function SelectMapPosition() {
   const navigation = useNavigation();
