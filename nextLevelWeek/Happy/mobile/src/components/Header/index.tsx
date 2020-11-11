bb65a43cd6e3d41699f96fafadb2565c050af7b4
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-10 22:55:25 -0300

    adicioando botoes ao header

--- nextLevelWeek/Happy/mobile/src/components/Header/index.tsx
+++ nextLevelWeek/Happy/mobile/src/components/Header/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import {Feather} from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 interface HeaderProps {
     title: string
 }
 
 export default function Header({title}: HeaderProps) {
+
+    const navigation = useNavigation()
+
+    function handleGoToHomePage(){
+        navigation.navigate('OrphanagesMap')
+    }
+
   return <View style={styles.container}>
+      <BorderlessButton onPress={navigation.goBack} >
+        <Feather name="arrow-left" size={24} color="#15b6d6"/>
+      </BorderlessButton>
+
       <Text style={styles.title}>{title}</Text>
+
+      <BorderlessButton onPress={handleGoToHomePage} >
+        <Feather name="x" size={24} color="#ff669d"/>
+      </BorderlessButton>
   </View>
 }
 
