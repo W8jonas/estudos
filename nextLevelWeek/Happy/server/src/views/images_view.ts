480ffce20f773f046074eb0bdd24c5d1355636f0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-12 19:16:48 -0300

    alterando de locallhost para ip local no backend

--- nextLevelWeek/Happy/server/src/views/images_view.ts
+++ nextLevelWeek/Happy/server/src/views/images_view.ts
@@ -5,7 +5,7 @@ export default {
     render (image: Image) {
         return {
             id: image.id,
-            url: `http://localhost:3333/uploads/${image.path}`
+            url: `http://192.168.2.2:3333/uploads/${image.path}`
         }
     },
 
