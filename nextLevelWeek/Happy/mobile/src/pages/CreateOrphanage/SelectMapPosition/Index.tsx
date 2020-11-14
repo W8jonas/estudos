aaf3527000184217778e7f3dcc23036412bc07c4
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-11-13 23:32:34 -0300

    colocando  opcao de selecionar posicao no mapa

--- nextLevelWeek/Happy/mobile/src/pages/CreateOrphanage/SelectMapPosition/Index.tsx
+++ nextLevelWeek/Happy/mobile/src/pages/CreateOrphanage/SelectMapPosition/Index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Dimensions, Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, MapEvent } from 'react-native-maps';
 
 import mapMarkerImg from '../../../images/map-marker.png';
 
 export default function SelectMapPosition() {
   const navigation = useNavigation();
 
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0})
+  
   function handleNextStep() {
-    navigation.navigate('OrphanageData');
+    navigation.navigate('OrphanageData', {position});
+  }
+  
+  function handleSelectMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate)
   }
 
   return (
@@ -24,16 +30,17 @@ export default function SelectMapPosition() {
           longitudeDelta: 0.008,
         }}
         style={styles.mapStyle}
+        onPress={handleSelectMapPosition}
       >
-        <Marker 
+        {position.latitude !== 0 && <Marker 
           icon={mapMarkerImg}
-          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
-        />
+          coordinate={position}
+        />}
       </MapView>
 
-      <RectButton style={styles.nextButton} onPress={handleNextStep}>
+      {position.latitude !== 0 && <RectButton style={styles.nextButton} onPress={handleNextStep}>
         <Text style={styles.nextButtonText}>Próximo</Text>
-      </RectButton>
+      </RectButton>}
     </View>
   )
 }
