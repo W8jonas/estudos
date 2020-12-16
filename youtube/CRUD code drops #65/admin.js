3c200552bae4e90cd7f67db105afc263f251fcb7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-12-15 23:55:27 -0300

    terminando parte do youtube

--- youtube/CRUD code drops #65/admin.js
+++ youtube/CRUD code drops #65/admin.js
@@ -1,23 +1,67 @@
 
+const mongoose = require('mongoose')
+
+const ProjectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    }, 
+    description: String,
+    completed: Boolean,
+    createdAt: {
+        type: Date, 
+        default: Date.now
+    }
+})
+
+const Project = mongoose.model("Project", ProjectSchema)
+
+
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
 
-const adminBro = new AdminBro({
-    database: [],
-    rootPath: '/admin'
+AdminBro.registerAdapter(AdminBroMongoose)
+
+
+const adminBroOptions = new AdminBro({
+	resources: [
+    { resource: Project, options: {
+      properties: {
+        description: { type: 'richtext' },
+        created_at: {
+          isVisible: { edit: false, list: true, show: true, filter: true }
+        }
+      }
+   }},
+  ],
+  locale: {
+    translations: {
+      labels: {
+        Project: 'Meus projetos'
+      }
+    }
+  },
+  rootPath: '/admin'
 })
 
 
-const router = AdminBroExpress.buildRouter(adminBro)
+const router = AdminBroExpress.buildRouter(adminBroOptions)
+
 
 
 const express = require('express')
 const server = express()
 
-server
-    .use(adminBro.options.rootPath, router)
-    .listen(3333, ()=> console.log('Server started'))
+server.use(adminBroOptions.options.rootPath, router)
 
 
+const run = async () => {
+    await mongoose.connect("mongodb://localhost/adminbroapp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    await server.listen(3333, () => console.log('Server started'))
+}
 
-
+run()
