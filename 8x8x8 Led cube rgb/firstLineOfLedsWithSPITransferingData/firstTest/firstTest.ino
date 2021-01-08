04e1e11345fd3a35735a4f94aac9424d6a2b4d7a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-08 04:10:32 -0300

    Corrigindo codigo
    trocando pino de latch (Erro de delay no togle deste pino)
    Removendo funcao antiga e reconfigurando SPI

--- 8x8x8 Led cube rgb/firstLineOfLedsWithSPITransferingData/firstTest/firstTest.ino
+++ 8x8x8 Led cube rgb/firstLineOfLedsWithSPITransferingData/firstTest/firstTest.ino
@@ -1,33 +1,35 @@
 #include <SPI.h>
 
-int latchPin = 12; // Pin connected to ST_CP of 74HC595
+int latchPin = 8;  // Pin connected to ST_CP of 74HC595
 int clockPin = 13; // Pin connected to SH_CP of 74HC595
-int dataPin = 11; // Pin connected to DS of 74HC595
+int dataPin = 11;  // Pin connected to DS of 74HC595
 
 void setup() {
+  SPI.setBitOrder(LSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+  
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  
+
   SPI.begin();
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setClockDivider(SPI_CLOCK_DIV128);
 }
 
 void loop() {
+  digitalWrite(latchPin, LOW);
   SPI.transfer(0xAA);
-  SPI.transfer(0x11);
-  SPI.transfer(0x22);
+  SPI.transfer(0x0A);
+  SPI.transfer(0x88);
+  SPI.transfer(0x99);
   digitalWrite(latchPin, HIGH);
-  delay(5);
   digitalWrite(latchPin, LOW);
-  delay(500);
+  delay(1000);
   
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
+  SPI.transfer(0x77);
   digitalWrite(latchPin, HIGH);
-  delay(5);
   digitalWrite(latchPin, LOW);
-  delay(500);
+  delay(1000);
+  
+  SPI.end();
 }
