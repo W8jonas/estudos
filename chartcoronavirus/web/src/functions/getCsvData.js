3db417b00a776c0802ff8a7ea10975b9aaa61e6d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-25 01:39:36 -0300

    Adicionando novo banco de dados atualizado e referenciando os créditos

--- chartcoronavirus/web/src/functions/getCsvData.js
+++ chartcoronavirus/web/src/functions/getCsvData.js
@@ -1,10 +1,19 @@
 
 import Papa from 'papaparse';
 
+/*
+ * 
+ * A tabela CSV é extraída deste repositório, todos os direitos reservados aos produtores.
+ * 
+ * https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data
+ * 
+ */
+
+
 export default async function useAsyncHook() {
 
     async function fetchCsv() {
-        const response = await fetch('newData.csv');
+        const response = await fetch('time_series_covid19_confirmed_global.csv');
         const reader = response.body.getReader();
         const result = await reader.read();
         const decoder = new TextDecoder('utf-8');
