23339f658865150701a5e53ab61c58dc2b1a9149
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-26 20:39:21 -0300

    Separando  componente Widget

--- Alura/imersaoReactNextJs/eletronicaquiz/pages/index.js
+++ Alura/imersaoReactNextJs/eletronicaquiz/pages/index.js
@@ -1,10 +1,57 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/widget'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Aqui vem o titulo</h1>
+            </Widget.Header>
+            <p>Aqui vem um parágrafo para uma pergunta</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Aqui vem o titulo</h1>
+            </Widget.Header>
+            <p>Aqui vem um parágrafo para uma pergunta</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+
+    </BackgroundImage>
+
+  )
 }
---
+++ Alura/imersaoReactNextJs/eletronicaquiz/src/components/widget/index.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caF50;
+  background-color: #1c1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+export default Widget
