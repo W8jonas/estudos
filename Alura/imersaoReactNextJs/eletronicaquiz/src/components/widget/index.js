7a9df7a34ff1ad8ea32652d328c40705dc8bbef2
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-26 20:54:29 -0300

    ADD comp Widget

--- Alura/imersaoReactNextJs/eletronicaquiz/src/components/widget/index.js
+++ Alura/imersaoReactNextJs/eletronicaquiz/src/components/widget/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
-  border: 1px solid #4caF50;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
   background-color: #1c1814;
   border-radius: 4px;
   overflow: hidden;
