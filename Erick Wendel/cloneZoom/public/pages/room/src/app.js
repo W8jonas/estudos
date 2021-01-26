6a921ced7d03a7ab69582d98054cea83a0d46b15
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-26 01:51:02 -0300

    Renderizando Video dos participantes da chamada

--- Erick Wendel/cloneZoom/public/pages/room/src/app.js
+++ Erick Wendel/cloneZoom/public/pages/room/src/app.js
@@ -11,10 +11,15 @@ const recordClick = function (recorderBtn) {
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
-  console.log('this is the room', room)
+  console.log('this is the room:', room)
 
-  const recorderBtn = document.getElementById('record')
-  recorderBtn.addEventListener('click', recordClick(recorderBtn))
+  // const recorderBtn = document.getElementById('record')
+  // recorderBtn.addEventListener('click', recordClick(recorderBtn))
+
+  const view = new View()
+  view.renderVideo({ userId: 'Random 1', url: 'https://media.giphy.com/media/HOhtUWb2tOWSoM1W5h/giphy.mp4' })
+  view.renderVideo({ userId: 'Random 2', url: 'https://media.giphy.com/media/HOhtUWb2tOWSoM1W5h/giphy.mp4' })
+  view.renderVideo({ userId: 'W8Jonas', isCurrentId: true, url: 'https://media.giphy.com/media/HOhtUWb2tOWSoM1W5h/giphy.mp4' })
 
 }
 
