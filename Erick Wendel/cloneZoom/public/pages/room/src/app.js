d2e4e6c1e14799888deeb008a05bca7dec5c1fbf
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-27 17:32:55 -0300

    criando objeto de configs do peer

--- Erick Wendel/cloneZoom/public/pages/room/src/app.js
+++ Erick Wendel/cloneZoom/public/pages/room/src/app.js
@@ -17,6 +17,17 @@ const onload = () => {
   // recorderBtn.addEventListener('click', recordClick(recorderBtn))
   const socketUrl = 'http://localhost:3000'
   const socketBuilder = new SocketBuilder({ socketUrl })
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/'
+    }
+  })
+  const peerBuilder = new PeerBuilder({ peerConfig })
+
   const view = new View()
   const media = new Media()
 
