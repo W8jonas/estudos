8ba6fa187fa3adf1124f6cdf164d8dc4379f374b
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-27 02:38:00 -0300

    criando componente ProgressBar

---
+++ chartcoronavirus/web/src/components/ProgressBar/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './styles.css'
+
+const ProgressBar = ({ maxValue = 100, actualValue = 50 }) => {
+
+    const value = (actualValue > maxValue) ? maxValue : actualValue
+    const progress = value / maxValue
+    const progressInPercentage = progress * 100
+
+    return (
+        <div className="content-container">
+
+            <div>
+                <div style={{ width: `${progressInPercentage}%`, height: 13, backgroundColor: '#774192' }} />
+            </div>
+
+            <p>
+                {progressInPercentage.toFixed(0)}%
+            </p>
+
+        </div>
+    )
+}
+
+export default ProgressBar
