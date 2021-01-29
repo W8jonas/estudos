da228ffbb11e86280983b527b088eb141d476967
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-01-29 02:22:43 -0300

    ADD eventos quando a chamada é fechada ou recebe erro

--- Erick Wendel/cloneZoom/public/pages/room/src/business.js
+++ Erick Wendel/cloneZoom/public/pages/room/src/business.js
@@ -31,9 +31,11 @@ class Business {
             .setOnConnectionOpened(this.onPeerConnectionOpened())
             .setOnCallReceived(this.onPeerCallReceived())
             .setOnPeerStreamReceived(this.onPeerStreamReceived())
+            .setOnCallError(this.onPeerCallError())
+            .setOnCallClose(this.onPeerCallClose())
             .build()
 
-        this.addVideoStream('teste01')
+        this.addVideoStream(this.currentPeer.id)
     }
 
     addVideoStream(userId, stream = this.currentStream) {
@@ -57,6 +59,13 @@ class Business {
     onUserDisconnected = function () {
         return userId => {
             console.log('user disconnected', userId)
+
+            if (this.peers.has(userId)) {
+                this.peers.get(userId).call.close()
+                this.peers.delete(userId)
+            }
+            this.view.setParticipants(this.peers.size)
+            this.view.removeVideoElement(userId)
         }
     }
 
@@ -87,6 +96,18 @@ class Business {
 
             this.peers.set(callerId, { call })
             this.view.setParticipants(this.peers.size)
+        }
+    }
+
+    onPeerCallError = function () {
+        return (call, error) => {
+            console.log('an call error ocurred!', error)
+            this.view.removeVideoElement(call.peer)
+        }
+    }
+    onPeerCallClose = function () {
+        return call => {
+            console.log('an call close!', call.peer)
         }
     }
 }
