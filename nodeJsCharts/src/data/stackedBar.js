82f1994586583ee8df833ef70d46daad0be28967
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-10 02:29:45 -0300

    adicionando dados para dupla barra

--- nodeJsCharts/src/data/stackedBar.js
+++ nodeJsCharts/src/data/stackedBar.js
@@ -1,12 +1,127 @@
+const d3 = require('d3')
+const timeConv = d3.timeParse("%m-%d-%Y")
+
 module.exports = {
     stackedBarData: [
-        { date: timeConv('05-07-2021'), initialValue: 20, finalValue: 27 },
-        { date: timeConv('05-08-2021'), initialValue: 15, finalValue: 48 },
-        { date: timeConv('05-09-2021'), initialValue: 20, finalValue: 27 },
-        { date: timeConv('05-10-2021'), initialValue: 15, finalValue: 48 },
-        { date: timeConv('05-11-2021'), initialValue: 20, finalValue: 27 },
-        { date: timeConv('05-12-2021'), initialValue: 15, finalValue: 48 },
-        { date: timeConv('05-13-2021'), initialValue: 20, finalValue: 27 },
-        { date: timeConv('05-14-2021'), initialValue: 15, finalValue: 48 },
+        {
+            language: 'Rust',
+            initialValue: 78.9,
+            finalValue: 80,
+            color: '#000000'
+        },
+        {
+            language: 'Kotlin',
+            initialValue: 75.1,
+            finalValue: 80,
+            color: '#00a2ee'
+        },
+        {
+            language: 'Python',
+            initialValue: 68.0,
+            finalValue: 80,
+            color: '#fbcb39'
+        },
+        {
+            language: 'TypeScript',
+            initialValue: 67.0,
+            finalValue: 80,
+            color: '#007bc8'
+        },
+        {
+            language: 'Go',
+            initialValue: 65.6,
+            finalValue: 80,
+            color: '#65cedb'
+        },
+        {
+            language: 'Swift',
+            initialValue: 65.1,
+            finalValue: 80,
+            color: '#ff6e52'
+        },
+        {
+            language: 'JavaScript',
+            initialValue: 61.9,
+            finalValue: 80,
+            color: '#f9de3f'
+        },
+        {
+            language: 'C#',
+            initialValue: 60.4,
+            finalValue: 80,
+            color: '#5d2f8e'
+        },
+        {
+            language: 'F#',
+            initialValue: 59.6,
+            finalValue: 80,
+            color: '#008fc9'
+        },
+        {
+            language: 'Clojure',
+            initialValue: 59.6,
+            finalValue: 80,
+            color: '#507dca'
+        },
+        {
+            language: 'Rust',
+            initialValue: 78.9 - 50,
+            finalValue: 80 - 30,
+            color: '#000000'
+        },
+        {
+            language: 'Kotlin',
+            initialValue: 75.1 - 50,
+            finalValue: 80 - 30,
+            color: '#00a2ee'
+        },
+        {
+            language: 'Python',
+            initialValue: 68.0 - 50,
+            finalValue: 80 - 30,
+            color: '#fbcb39'
+        },
+        {
+            language: 'TypeScript',
+            initialValue: 67.0 - 50,
+            finalValue: 80 - 30,
+            color: '#007bc8'
+        },
+        {
+            language: 'Go',
+            initialValue: 65.6 - 50,
+            finalValue: 80 - 30,
+            color: '#65cedb'
+        },
+        {
+            language: 'Swift',
+            initialValue: 65.1 - 50,
+            finalValue: 80 - 30,
+            color: '#ff6e52'
+        },
+        {
+            language: 'JavaScript',
+            initialValue: 61.9 - 50,
+            finalValue: 80 - 30,
+            color: '#f9de3f'
+        },
+        {
+            language: 'C#',
+            initialValue: 60.4 - 50,
+            finalValue: 80 - 30,
+            color: '#5d2f8e'
+        },
+        {
+            language: 'F#',
+            initialValue: 59.6 - 50,
+            finalValue: 80 - 30,
+            color: '#008fc9'
+        },
+        {
+            language: 'Clojure',
+            initialValue: 59.6 - 50,
+            finalValue: 80 - 30,
+            color: '#507dca'
+        }
     ]
 }
