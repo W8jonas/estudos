9a2c0e57bc5539af2ddcb633a1525ad265302fd1
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-17 19:25:58 -0300

    colocando variáveis em ingles

--- estudo de POO com python/main.py
+++ estudo de POO com python/main.py
@@ -1,38 +1,38 @@
-class Funcionario:
-    salario_minimo = 1000
+class Employee:
+    minimum_wage = 1000
 
-    def __init__(self, nome, idade, meses_na_empresa, cargo):
-        self.nome = nome
-        self.idade = idade
-        self.meses_na_empresa = meses_na_empresa
-        self.cargo = cargo
+    def __init__(self, name, age, monthsInTheCompany, postInCompany):
+        self.name = name
+        self.age = age
+        self.monthsInTheCompany = monthsInTheCompany
+        self.postInCompany = postInCompany
 
-    def mostrar_nome(self):
-        print(self.nome)
+    def show_name(self):
+        print(self.name)
 
-    def mostrar_nome_e_idade(self):
-        print(self.nome, self.idade)
+    def show_name_and_age(self):
+        print(self.name, self.age)
 
-    def calcular_salario(self):
-        bonificação_de_programador = 400
-        bonificação_de_analista = 600
-        bonificação_de_gerente = 1000
+    def calculate_salary(self):
+        developer_bonus = 400
+        analyst_bonus = 600
+        manager_bonus = 1000
 
-        if (self.cargo == "programador"):
-            print(self.salario_minimo * self.meses_na_empresa + bonificação_de_programador)
-        elif (self.cargo == "analista"):
-            print(self.salario_minimo * self.meses_na_empresa + bonificação_de_analista)
-        elif (self.cargo == "gerente"):
-            print(self.salario_minimo * self.meses_na_empresa + bonificação_de_gerente)
+        if (self.postInCompany == "programador"):
+            print(self.minimum_wage * self.monthsInTheCompany + developer_bonus)
+        elif (self.postInCompany == "analista"):
+            print(self.minimum_wage * self.monthsInTheCompany + analyst_bonus)
+        elif (self.postInCompany == "gerente"):
+            print(self.minimum_wage * self.monthsInTheCompany + manager_bonus)
         else:
-            print(self.salario_minimo * self.meses_na_empresa)
+            print(self.minimum_wage * self.monthsInTheCompany)
 
 
-primeiro_funcionario = Funcionario("Jonas", 19, 3, "programador")
-primeiro_funcionario.calcular_salario()
+programmer = Employee("Jonas", 19, 3, "programador")
+programmer.calculate_salary()
 
-segundo_funcionario = Funcionario("Pablo", 17, 3, "analista")
-segundo_funcionario.calcular_salario()
+analyst = Employee("Pablo", 17, 3, "analista")
+analyst.calculate_salary()
 
-terceiro_funcionario = Funcionario("Lucas", 20, 3, "gerente")
-terceiro_funcionario.calcular_salario()
+manager = Employee("Lucas", 20, 3, "gerente")
+manager.calculate_salary()
