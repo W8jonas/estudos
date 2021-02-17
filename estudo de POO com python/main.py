5ba6ffbc42a7e6911879bf1be7569a8c8a8a3ea7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-17 19:08:50 -0300

    adicionando novos metodos a classe Funcionario

--- estudo de POO com python/main.py
+++ estudo de POO com python/main.py
@@ -1,12 +1,38 @@
-
 class Funcionario:
+    salario_minimo = 1000
 
-    def __init__(self, nome):
+    def __init__(self, nome, idade, meses_na_empresa, cargo):
         self.nome = nome
+        self.idade = idade
+        self.meses_na_empresa = meses_na_empresa
+        self.cargo = cargo
 
     def mostrar_nome(self):
         print(self.nome)
 
+    def mostrar_nome_e_idade(self):
+        print(self.nome, self.idade)
 
-primeiro_funcionario = Funcionario("Jonas")
-primeiro_funcionario.mostrar_nome()
+    def calcular_salario(self):
+        bonificação_de_programador = 400
+        bonificação_de_analista = 600
+        bonificação_de_gerente = 1000
+
+        if (self.cargo == "programador"):
+            print(self.salario_minimo * self.meses_na_empresa + bonificação_de_programador)
+        elif (self.cargo == "analista"):
+            print(self.salario_minimo * self.meses_na_empresa + bonificação_de_analista)
+        elif (self.cargo == "gerente"):
+            print(self.salario_minimo * self.meses_na_empresa + bonificação_de_gerente)
+        else:
+            print(self.salario_minimo * self.meses_na_empresa)
+
+
+primeiro_funcionario = Funcionario("Jonas", 19, 3, "programador")
+primeiro_funcionario.calcular_salario()
+
+segundo_funcionario = Funcionario("Pablo", 17, 3, "analista")
+segundo_funcionario.calcular_salario()
+
+terceiro_funcionario = Funcionario("Lucas", 20, 3, "gerente")
+terceiro_funcionario.calcular_salario()
