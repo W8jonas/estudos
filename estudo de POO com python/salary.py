f62d86d2eb48952373575f9a80b5aeabb68d1a5e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-20 23:20:39 -0300

    criando metodo get na classe salario

--- estudo de POO com python/salary.py
+++ estudo de POO com python/salary.py
@@ -1,25 +1,26 @@
 class Salary:
-    minimum_wage = 1000
     
     def __init__(self, monthsInTheCompany, postInCompany):
+        self.minimum_wage = 1000
         self.actual_salary = 0
         self.monthsInTheCompany = monthsInTheCompany
         self.postInCompany = postInCompany
 
-    def __calculate_salary(self):
+    def calculate(self):
         developer_bonus = 400
         analyst_bonus = 600
         manager_bonus = 1000
 
         if (self.postInCompany == "programador"):
-            print(self.minimum_wage * self.monthsInTheCompany + developer_bonus)
+            self.actual_salary = self.minimum_wage * self.monthsInTheCompany + developer_bonus
         elif (self.postInCompany == "analista"):
-            print(self.minimum_wage * self.monthsInTheCompany + analyst_bonus)
+            self.actual_salary = self.minimum_wage * self.monthsInTheCompany + analyst_bonus
         elif (self.postInCompany == "gerente"):
-            print(self.minimum_wage * self.monthsInTheCompany + manager_bonus)
+            self.actual_salary = self.minimum_wage * self.monthsInTheCompany + manager_bonus
         else:
-            print(self.minimum_wage * self.monthsInTheCompany)
+            self.actual_salary = self.minimum_wage * self.monthsInTheCompany
     
-    def calculate(self):
-        self.__calculate_salary()
+    def get(self):
+        return self.actual_salary
+
 
