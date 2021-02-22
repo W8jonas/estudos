5ebb3451a3c3350e03ef56ed7e62352fa487e4ec
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-22 17:48:50 -0300

    adicionando nova rota para ler arquivo

--- rocketpay/lib/rocketpay_web/controllers/WelcomeController.ex
+++ rocketpay/lib/rocketpay_web/controllers/WelcomeController.ex
@@ -1,7 +1,24 @@
 defmodule RocketpayWeb.WelcomeController do
   use RocketpayWeb, :controller
 
-  def index(conn, _params) do
-    text(conn, "Welcome to Rocketpay api")
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
   end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Bem vindo ao rocketpay API. Aqui está seu número: #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
+  end
+
 end
--- rocketpay/lib/rocketpay_web/router.ex
+++ rocketpay/lib/rocketpay_web/router.ex
@@ -8,7 +8,7 @@ defmodule RocketpayWeb.Router do
   scope "/api", RocketpayWeb do
     pipe_through :api
 
-    get "/", WelcomeController, :index
+    get "/:filename", WelcomeController, :index
   end
 
   # Enables LiveDashboard only for development
