68648c9e6d8dc72fcbf0363e3c7217691c0353c0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-22 16:42:48 -0300

    criando pipe para calcular a soma dos numeros do arquivo

--- rocketpay/lib/rocketpay/numbers.ex
+++ rocketpay/lib/rocketpay/numbers.ex
@@ -1,9 +1,18 @@
 defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
-    file = File.read("#{filename}.csv")
-    handle_file(file)
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, stringArray}) do
+    stringArray = stringArray
+    |> String.split(",")
+    |> Enum.map(fn number -> String.to_integer(number) end)
+    |> Enum.sum()
+
+    {:ok, %{result: stringArray}}
+
+  end
   defp handle_file({:error, _reson}), do: {:error, "invalid file!"}
 end
