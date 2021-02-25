a88ac86084c31744ad789e20c6543f94c9480b46
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-25 18:54:16 -0300

    alterando account para receber updates

--- rocketpay/lib/rocketpay/account.ex
+++ rocketpay/lib/rocketpay/account.ex
@@ -16,8 +16,8 @@ defmodule Rocketpay.Account do
     timestamps()
   end
 
-  def changeset(params) do
-    %__MODULE__{}
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> check_constraint(:balance, name: :balance_must_be_positive_or_zero)
