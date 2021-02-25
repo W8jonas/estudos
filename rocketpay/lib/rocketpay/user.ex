ecc341e3a3c3294d5a427a877f4776c22f699649
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-25 16:01:14 -0300

    relacionando usuario para conta

--- rocketpay/lib/rocketpay/user.ex
+++ rocketpay/lib/rocketpay/user.ex
@@ -3,6 +3,7 @@ defmodule Rocketpay.User do
 
   import Ecto.Changeset
   alias Ecto.Changeset
+  alias Rocketpay.Account
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
@@ -15,6 +16,8 @@ defmodule Rocketpay.User do
     field :nickname, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+
+    has_one :account, Account
 
     timestamps()
   end
