9d6396847a7261f6da6f2237fa4d7841acbcbd91
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-25 16:00:39 -0300

    criando schema para account

---
+++ rocketpay/lib/rocketpay/account.ex
@@ -0,0 +1,26 @@
+defmodule Rocketpay.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Rocketpay.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @required_params [:balance, :user_id]
+
+  schema "accounts" do
+    field :balance, :decimal
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> check_constraint(:balance, name: :balance_must_be_positive_or_zero)
+  end
+
+end
