a07be689276715787d1e5ef336f8e95ebdf2292f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-25 13:48:25 -0300

    lidando com erros

--- rocketpay/lib/rocketpay_web/views/error_view.ex
+++ rocketpay/lib/rocketpay_web/views/error_view.ex
@@ -13,4 +13,17 @@ defmodule RocketpayWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("400.json", %{result: %Ecto.Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
+
 end
