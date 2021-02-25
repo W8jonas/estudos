8207c30a4197ccfc4bf360922c99ea75cf7c4a3a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-25 17:45:37 -0300

    fazendo etapa de view para mostrar dados da conta

--- rocketpay/lib/rocketpay_web/views/users_view.ex
+++ rocketpay/lib/rocketpay_web/views/users_view.ex
@@ -1,14 +1,28 @@
 defmodule RocketpayWeb.UsersView do
 
-  alias Rocketpay.User
+  alias Rocketpay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+    user: %User{
+      id: id,
+      name: name,
+      nickname: nickname,
+      account: %Account{
+        id: account_id,
+        balance: balance
+      }
+    }
+    }) do
     %{
       nessage: "User created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
