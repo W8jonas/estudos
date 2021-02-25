55e3984ff2e9f5b9747ac127b48948e5ae8149f0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-25 17:39:16 -0300

    criando etapa de criar conta + usuários simulteneamente

--- rocketpay/lib/rocketpay/users/create.ex
+++ rocketpay/lib/rocketpay/users/create.ex
@@ -1,10 +1,37 @@
 defmodule Rocketpay.User.Create do
-  alias Rocketpay.{Repo, User}
+  alias Rocketpay.{Repo, User, Account}
+
+  alias Ecto.Multi
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} -> insert_account(repo, user.id) end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} -> preload_data(repo, user) end)
+    |> run_transation()
   end
 
+  defp insert_account(repo, user_id) do
+    user_id
+    |> account_changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(user_id) do
+    params = %{user_id: user_id, balance: "0.00"}
+
+    Account.changeset(params)
+  end
+
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_transation(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
+
+  end
 end
