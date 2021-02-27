efc2d5d1cd5a55e41bef5e20dbf49df98fcd95a7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-26 23:59:09 -0300

    add decimal dep

--- rocketpay/mix.exs
+++ rocketpay/mix.exs
@@ -44,7 +44,8 @@ defmodule Rocketpay.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:pbkdf2_elixir, "~> 1.3"}
+      {:pbkdf2_elixir, "~> 1.3"},
+      {:decimal, "~> 2.0"}
     ]
   end
 
