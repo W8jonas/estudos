9f8a82397faca73a786df0d8254715692373bdd7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-27 22:04:04 -0300

    add struct Transaction

---
+++ rocketpay/lib/rocketpay/accouts/transactions/response.ex
@@ -0,0 +1,12 @@
+defmodule Rocketpay.Accounts.Transaction.Response do
+
+  alias Rocketpay.Account
+  defstruct [:from_account, :to_account]
+
+  def build(%Account{} = from_account, %Account{} = to_account) do
+    %__MODULE__{
+      from_account: from_account,
+      to_account: to_account
+    }
+  end
+end
