65de58051f9e60db80bf6181be205a872cc17651
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-02-27 21:23:53 -0300

    add transaction na facade

--- rocketpay/lib/rocketpay.ex
+++ rocketpay/lib/rocketpay.ex
@@ -1,11 +1,12 @@
 defmodule Rocketpay do
 
   alias Rocketpay.User.Create, as: UserCreate
-  alias Rocketpay.Accounts.{Deposit, Withdraw}
+  alias Rocketpay.Accounts.{Deposit, Transaction, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
-
   defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
+
 end
