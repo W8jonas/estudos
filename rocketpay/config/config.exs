9e3a72297748ffec61c2611645f3e942a07098e1
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-01 18:31:39 -0300

    adicionando config para user e password para API

--- rocketpay/config/config.exs
+++ rocketpay/config/config.exs
@@ -22,6 +22,12 @@ config :rocketpay, Rocketpay.Repo,
   migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
 
+
+config :rocketpay, :basic_auth,
+  username: "pera",
+  password: "geladinha"
+
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
