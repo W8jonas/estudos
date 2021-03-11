1687f447a8755de7b20290207f15416caeadbdd9
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-10 23:39:52 -0300

    criando index do projeto

---
+++ arquitetura e padroes de projeto/todoApp/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Hello App </Text>
+    </View>
+  );
+};
+
+export default App;
