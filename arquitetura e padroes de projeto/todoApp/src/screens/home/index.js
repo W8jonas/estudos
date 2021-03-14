b9f1d12368f2f0135cfb49a48e90924b0aab6fc4
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-14 02:51:53 -0300

    utilizando componente task

--- arquitetura e padroes de projeto/todoApp/src/screens/home/index.js
+++ arquitetura e padroes de projeto/todoApp/src/screens/home/index.js
@@ -8,37 +8,16 @@ import { View, Text, TouchableOpacity } from 'react-native'
 // Functions
 
 // Components
+import Task from '../../components/task'
 
 function Home() {
 	return (
-		<View style={{ padding: 10, flex: 1, backgroundColor: '#FFF' }}>
+		<View style={{ padding: 10, flex: 1, backgroundColor: '#eee' }}>
 
-			<View style={{
-				width: '100%',
-				height: 60,
-				backgroundColor: '#FCF',
-				elevation: 9,
-				borderBottomWidth: 3,
-				borderBottomColor: '#ccc',
-				flexDirection: 'row',
-				alignItems: 'center',
-				paddingHorizontal: 15,
-				borderRadius: 30,
-			}}
-			>
-				<View style={{
-					height: 30, width: 30, borderRadius: 30, borderWidth: 3, borderColor: '#f60', backgroundColor: '#Fa0',
-				}}
-				/>
-
-				<View style={{
-					flexDirection: 'row', justifyContent: 'space-between', width: '90%', marginLeft: 10,
-				}}
-				>
-					<Text style={{ width: '70%' }}>Descrição</Text>
-					<Text style={{ width: '20%' }}>Data</Text>
-				</View>
-			</View>
+			<Task />
+			<Task />
+			<Task />
+			<Task />
 
 		</View>
 	)
