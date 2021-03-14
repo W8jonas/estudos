6413a722cbfafd8b7daa81fa44bf548145e43ca0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-13 23:18:27 -0300

    criando tela Home

---
+++ arquitetura e padroes de projeto/todoApp/src/screens/home/index.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+function Home() {
+	return (
+		<View>
+			<Text>Home</Text>
+		</View>
+	)
+}
+
+export default Home
