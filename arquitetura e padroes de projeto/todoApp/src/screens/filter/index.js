8eb055aac55aa15f9983436a6f95a943fd3a443f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-13 23:18:36 -0300

    criando tela Filter

---
+++ arquitetura e padroes de projeto/todoApp/src/screens/filter/index.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+function Filter() {
+	return (
+		<View>
+			<Text>Filter</Text>
+		</View>
+	)
+}
+
+export default Filter
