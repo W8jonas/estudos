f409a8a74ecdde01c5a4352645668238f443c7fd
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-26 23:50:29 -0300

    colocando estilo separadamente

---
+++ arquitetura e padroes de projeto/todoApp/src/components/task/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+	container: {
+		width: '100%',
+		height: 60,
+		backgroundColor: '#FFF',
+		elevation: 4,
+		borderBottomWidth: 2,
+		borderBottomColor: '#ccc',
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingHorizontal: 15,
+		borderRadius: 30,
+		marginTop: 10,
+	},
+	textContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: '90%',
+		marginLeft: 10,
+	},
+	checkCircle: {
+		height: 30,
+		width: 30,
+		borderRadius: 30,
+		borderWidth: 3,
+		borderColor: '#f60',
+		backgroundColor: '#Fa0',
+	},
+	textDescription: {
+		width: '70%',
+	},
+	textDate: {
+		width: '20%',
+	},
+})
+
+export default styles
