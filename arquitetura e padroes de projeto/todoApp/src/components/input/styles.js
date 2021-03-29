cbc55b961f90cbdd34b6893794b1c18da875d2f6
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-29 19:06:12 -0300

    refinando estilos comp Input

--- arquitetura e padroes de projeto/todoApp/src/components/input/styles.js
+++ arquitetura e padroes de projeto/todoApp/src/components/input/styles.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Dimensions } from 'react-native'
 
+import { colors } from '../../styles'
+
 const { width: screenWidth } = Dimensions.get('window')
 
 const widthContainer = screenWidth - 2 * 10
 
 const styles = StyleSheet.create({
 	container: {
-		backgroundColor: '#FFF',
+		backgroundColor: colors.fullWhite,
 		flexDirection: 'row',
 		alignItems: 'center',
 		borderRadius: 22,
@@ -16,32 +18,38 @@ const styles = StyleSheet.create({
 		marginBottom: 5,
 	},
 	textInput: {
-		width: widthContainer * 0.6,
+		width: widthContainer * 0.49,
 	},
 	touchCalendar: {
 		alignItems: 'center',
 		justifyContent: 'center',
-		backgroundColor: '#ccc',
+		backgroundColor: colors.whiteMetallic,
 		marginHorizontal: 5,
+		borderRadius: 12,
 		padding: 5,
-		height: 30,
-		// width: 40,
-		width: widthContainer * 0.1,
+		height: 34,
+		width: 34,
 	},
 	touchTypeOfTask: {
 		flexDirection: 'row',
-		justifyContent: 'center',
+		justifyContent: 'space-between',
 		alignItems: 'center',
-		backgroundColor: '#eaebee',
-		width: widthContainer * 0.23,
+		backgroundColor: colors.whiteMetallic,
+		paddingHorizontal: 5,
+		width: widthContainer * 0.38,
+		maxWidth: 140,
 		height: 30,
 	},
 	circle: {
 		height: 12,
 		width: 12,
 		borderRadius: 12,
-		borderColor: '#F60',
+		borderColor: colors.whiteDefault,
 		borderWidth: 2,
+	},
+	textTypeOfTask: {
+		fontSize: 16,
+		color: colors.grayDark,
 	},
 })
 
