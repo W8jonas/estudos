dcf5b07cc18c025f6bd166bac4bcbae4561d1d0c
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-29 18:32:23 -0300

    alterando estilo global de cores

--- arquitetura e padroes de projeto/todoApp/src/styles.js
+++ arquitetura e padroes de projeto/todoApp/src/styles.js
@@ -1,8 +1,11 @@
 const colors = {
 	fullWhite: '#FFF',
 	whiteSolid: '#E1E2E7',
+	whiteMetallic: '#f2f3f6',
 
 	blackSolid: '#555B69',
+	grayDark: '#646671',
+
 	blackDark: '#1E1E1F',
 
 	greySolid: '#B8BAC6',
