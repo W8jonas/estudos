f5eeb0529b60a04fc4980b1410c1a45901df5a42
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-29 19:06:26 -0300

    add mais cores à paleta

--- arquitetura e padroes de projeto/todoApp/src/styles.js
+++ arquitetura e padroes de projeto/todoApp/src/styles.js
@@ -2,13 +2,15 @@ const colors = {
 	fullWhite: '#FFF',
 	whiteSolid: '#E1E2E7',
 	whiteMetallic: '#f2f3f6',
+	whiteDefault: '#eaebee',
 
 	blackSolid: '#555B69',
 	grayDark: '#646671',
+	greySolid: '#B8BAC6',
 
 	blackDark: '#1E1E1F',
 
-	greySolid: '#B8BAC6',
+	greenDefault: '#080',
 }
 
 export {
