856ae914f2545dec2ebc7e067412d7d94b0ddadc
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-29 18:33:28 -0300

    add FAKE_DATA para tasks e componente Input

--- arquitetura e padroes de projeto/todoApp/src/screens/home/index.js
+++ arquitetura e padroes de projeto/todoApp/src/screens/home/index.js
@@ -8,17 +8,49 @@ import { View, Text, TouchableOpacity } from 'react-native'
 // Functions
 
 // Components
+import Input from '../../components/input'
 import Task from '../../components/task'
+
+const FAKE_DATA = [
+	{
+		id: '0',
+		description: 'Arrumar quarto',
+		type: '',
+		data: new Date('2021/03/29').getTime(),
+		done: false,
+	},
+	{
+		id: '1',
+		description: 'Terminar aplicativo',
+		type: 'programação',
+		data: new Date('2021/04/16').getTime(),
+		done: false,
+	},
+	{
+		id: '2',
+		description: 'Fazer componente Tarefa',
+		type: 'programação',
+		data: new Date('2021/03/27').getTime(),
+		done: true,
+	},
+	{
+		id: '3',
+		description: 'Estudar prova de Equações Diferenciais',
+		type: 'Faculdade',
+		data: new Date('2021/04/11').getTime(),
+		done: false,
+	},
+]
 
 function Home() {
 	return (
 		<View style={{ padding: 10, flex: 1, backgroundColor: '#eee' }}>
 
-			<Task />
-			<Task />
-			<Task />
-			<Task />
+			{FAKE_DATA.map((task) => (
+				<Task key={task.id} {...task} />
+			))}
 
+			<Input />
 		</View>
 	)
 }
