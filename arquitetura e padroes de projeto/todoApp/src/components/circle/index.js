98cccb8ee9a670d9f8d3825fdef33ba28905364b
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-03-30 18:37:45 -0300

    criando Circle peace

---
+++ arquitetura e padroes de projeto/todoApp/src/components/circle/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+// Assets
+import { colors } from '../../styles'
+
+const styles = StyleSheet.create({
+	checkCircle: {
+		height: 30,
+		width: 30,
+		borderRadius: 30,
+		borderWidth: 3,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderColor: colors.whiteDefault,
+		backgroundColor: colors.whiteDefault,
+	},
+})
+
+function Circle({ color, size }) {
+	return (
+		<View style={[styles.checkCircle, { borderColor: color, height: size, width: size }]} />
+	)
+}
+
+export default Circle
