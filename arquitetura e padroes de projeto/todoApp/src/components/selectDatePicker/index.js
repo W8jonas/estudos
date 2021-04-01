840a9772ba07add85b5412a6aaf8d3fe6a56f232
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-01 17:31:30 -0300

    adicionando funções, show e hide, e useEffect para chama-las

--- arquitetura e padroes de projeto/todoApp/src/components/selectDatePicker/index.js
+++ arquitetura e padroes de projeto/todoApp/src/components/selectDatePicker/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
-	View, Button, Text, Animated,
+	Button, Text, Animated, Easing, Dimensions,
 } from 'react-native'
 
 // Modules
@@ -10,9 +10,38 @@ import DatePicker from 'react-native-date-picker'
 // Assets
 import styles from './styles'
 
+const { height: screenHeight } = Dimensions.get('window')
+const heightOfContainer = screenHeight * 0.45
+
 function SelectDatePicker({ onCancel, visible, setDate }) {
 	const [datePicker, setDatePicker] = useState(new Date())
 	const [pos] = useState(new Animated.Value(0))
+
+	function show() {
+		Animated.timing(pos, {
+			toValue: heightOfContainer,
+			useNativeDriver: true,
+			duration: 200,
+			easing: Easing.linear,
+		}).start()
+	}
+
+	function hide() {
+		Animated.timing(pos, {
+			toValue: 0,
+			useNativeDriver: true,
+			duration: 200,
+			easing: Easing.linear,
+		}).start()
+	}
+
+	useEffect(() => {
+		if (visible) {
+			hide()
+		} else {
+			show()
+		}
+	}, [visible])
 
 	return (
 		<Animated.View
