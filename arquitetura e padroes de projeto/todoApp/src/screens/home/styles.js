ea6d936dabe1441ba6dbe826df27db34ae2a880d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-02 16:51:32 -0300

    criando estilos para tela Home

---
+++ arquitetura e padroes de projeto/todoApp/src/screens/home/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+import { colors } from '../../styles'
+
+const styles = StyleSheet.create({
+	touchContainer: {
+		backgroundColor: colors.fullWhite,
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderRadius: 22,
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		marginTop: 10,
+		marginBottom: 5,
+		width: '50%',
+		height: 30,
+	},
+})
+
+export default styles
