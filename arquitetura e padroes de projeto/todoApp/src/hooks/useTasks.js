5ef369779cf66716c3360010a30eec4f3c58fa51
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-03 18:15:26 -0300

    criando funçao addTasks

--- arquitetura e padroes de projeto/todoApp/src/hooks/useTasks.js
+++ arquitetura e padroes de projeto/todoApp/src/hooks/useTasks.js
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 
 function useTasks() {
-	const [task, setTask] = useState()
+	const [tasks, setTasks] = useState([])
 
-	return { task }
+	function addTask(taskToAdd) {
+		setTasks([...tasks, taskToAdd])
+	}
+
+	return { tasks, addTask }
 }
 
 export default useTasks
