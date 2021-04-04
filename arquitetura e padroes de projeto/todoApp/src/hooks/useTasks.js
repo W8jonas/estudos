0aacbde1c1a56b88c8f0261214d787ea91bcc8dd
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-03 23:23:35 -0300

    corrigindo erros e bugs no useTask

--- arquitetura e padroes de projeto/todoApp/src/hooks/useTasks.js
+++ arquitetura e padroes de projeto/todoApp/src/hooks/useTasks.js
@@ -5,28 +5,28 @@ const InitialState = [
 		id: '0',
 		description: 'Arrumar quarto',
 		type: '',
-		data: new Date('2021/03/29').getTime(),
+		date: new Date('2021/03/29').getTime(),
 		done: false,
 	},
 	{
 		id: '1',
 		description: 'Terminar aplicativo',
-		type: 'programação',
-		data: new Date('2021/04/16').getTime(),
+		type: 'Programação',
+		date: new Date('2021/04/16').getTime(),
 		done: false,
 	},
 	{
 		id: '2',
 		description: 'Fazer componente Tarefa',
-		type: 'programação',
-		data: new Date('2021/03/27').getTime(),
+		type: 'Programação',
+		date: new Date('2021/03/27').getTime(),
 		done: true,
 	},
 	{
 		id: '3',
 		description: 'Estudar prova de Equações Diferenciais',
 		type: 'Faculdade',
-		data: new Date('2021/04/11').getTime(),
+		date: new Date('2021/04/11').getTime(),
 		done: false,
 	},
 ]
@@ -35,7 +35,14 @@ function useTasks() {
 	const [tasks, setTasks] = useState(InitialState)
 
 	function addTask(taskToAdd) {
-		setTasks([...tasks, taskToAdd])
+		const newTask = {
+			date: taskToAdd.taskDate,
+			type: taskToAdd.taskType,
+			description: taskToAdd.description,
+			id: Math.random(),
+			done: false,
+		}
+		setTasks([...tasks, newTask])
 	}
 
 	return { tasks, addTask }
