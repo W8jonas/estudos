1373a3a2e446ebca0499c94754a48dfffd2b17b8
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-08 23:57:14 -0300

    iniciando comp filterTask

---
+++ arquitetura e padroes de projeto/todoApp/src/components/filterTask/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+function FilterTask() {
+	return (
+		<View>
+			<Text>FilterTask</Text>
+		</View>
+	)
+}
+
+export default FilterTask
