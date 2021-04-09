7e28602717b014dbcdef16df4224286fdda77d53
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-08 23:54:51 -0300

    adicionando tarefas na tela filter tasks

--- arquitetura e padroes de projeto/todoApp/src/screens/filter/index.js
+++ arquitetura e padroes de projeto/todoApp/src/screens/filter/index.js
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
-
-// Modules
-
-// Assets
-
-// Functions
+import {
+	View, Text, TouchableOpacity, ScrollView,
+} from 'react-native'
 
 // Components
+import Task from '../../components/task'
+
+// Functions
+import useTasks from '../../hooks/useTasks'
 
 function Filter() {
+	const {
+		tasks, addTask, toggleTaskDone, deleteTask, updateTask,
+	} = useTasks()
+
 	return (
 		<View>
-			<Text>Filter</Text>
+			<ScrollView>
+				{tasks.map((task) => (
+					<Task
+						key={task.id}
+						handleToggleTaskDone={toggleTaskDone}
+						handleDeleteTask={deleteTask}
+						handleUpdateTask={() => {}}
+						{...task}
+					/>
+				))}
+			</ScrollView>
 		</View>
 	)
 }
