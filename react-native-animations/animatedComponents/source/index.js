824a825fb7a7ff8a3a0c58e9a5680eb98c1ac2af
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-14 18:51:25 -0300

    adicionando primeira paleta de cores

--- react-native-animations/animatedComponents/source/index.js
+++ react-native-animations/animatedComponents/source/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { Button, View, Text } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
---
+++ react-native-animations/animatedComponents/source/screen/downloadButton/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+	white: '#FFF',
+	black: '#000',
+	green: '#0e7e70',
+	blue: '#246cfd',
+	lightGreen: '#1fc85c',
+}
