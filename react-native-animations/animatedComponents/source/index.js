622a67b7cf4ea371136a66752530d2504fd3f3c6
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-14 18:27:41 -0300

    criando stackNavigator

---
+++ react-native-animations/animatedComponents/source/index.js
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { Button, View, Text } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import DownloadButton from './screen/downloadButton/index'
+
+const Stack = createStackNavigator()
+
+function App() {
+	const defaultConfigForScreens = {
+		headerShown: false,
+	}
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="DownloadButton"
+					component={DownloadButton}
+					options={defaultConfigForScreens}
+				/>
+
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+}
+
+export default App
