9e47e020e79d3823b6d9a40702abe4a06076da27
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-14 19:34:24 -0300

    criando estrutura inicial

--- react-native-animations/animatedComponents/source/screen/downloadButton/index.js
+++ react-native-animations/animatedComponents/source/screen/downloadButton/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+// assets
+import styles from './styles'
+
 function DownloadButton() {
 	return (
-		<View>
-			<Text>DownloadButton</Text>
+		<View style={styles.screenContainer}>
+			<View style={styles.buttonContainer}>
+				<Text style={styles.iconContainer}>Icone</Text>
+				<View style={styles.textContainer}>
+					<Text style={styles.textContent}>Downloading</Text>
+				</View>
+			</View>
 		</View>
 	)
 }
