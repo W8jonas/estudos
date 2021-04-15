5f0fedfd32deb3e44808230085808186a6d62be9
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-15 13:15:22 -0300

    adicionando animação de downloading

--- react-native-animations/animatedComponents/source/screen/downloadButton/index.js
+++ react-native-animations/animatedComponents/source/screen/downloadButton/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+// Modules
+import LottieView from 'lottie-react-native'
+
 // assets
+import downloadAnimation from './assets/download-ongoing.json'
 import styles from './styles'
 
 function DownloadButton() {
 	return (
 		<View style={styles.screenContainer}>
 			<View style={styles.buttonContainer}>
-				<Text style={styles.iconContainer}>Icone</Text>
+				<View style={styles.iconContainer}>
+					<LottieView source={downloadAnimation} autoPlay loop speed={3} />
+				</View>
 				<View style={styles.textContainer}>
 					<Text style={styles.textContent}>Downloading</Text>
 				</View>
