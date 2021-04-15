85f2dc43cee71bae61d666b4c098efda18c77238
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-15 16:31:35 -0300

    criando icones animados

--- react-native-animations/animatedComponents/source/screen/downloadButton/index.js
+++ react-native-animations/animatedComponents/source/screen/downloadButton/index.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text } from 'react-native'
 
 // Modules
 import LottieView from 'lottie-react-native'
 
 // assets
+import completedDownloadAnimation from './assets/check-blue.json'
 import downloadAnimation from './assets/download-ongoing.json'
 import styles from './styles'
 
 function DownloadButton() {
+	const [animationGlobal, setAnimationGlobal] = useState(0)
+
+	const AnimationIcon = () => (
+		animationGlobal === 0
+			? <LottieView source={downloadAnimation} />
+			: animationGlobal === 1
+				? <LottieView source={downloadAnimation} autoPlay loop speed={3} />
+				: <LottieView source={completedDownloadAnimation} autoPlay loop speed={3} />
+	)
+
 	return (
 		<View style={styles.screenContainer}>
 			<View style={styles.buttonContainer}>
 				<View style={styles.iconContainer}>
-					<LottieView source={downloadAnimation} autoPlay loop speed={3} />
+					<AnimationIcon />
 				</View>
 				<View style={styles.textContainer}>
 					<Text style={styles.textContent}>Downloading</Text>
