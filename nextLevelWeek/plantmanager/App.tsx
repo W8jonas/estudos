7b082dceddaaad7c757fa10e35d409bfe005b90e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-20 13:00:26 -0300

    add useFonts arquivo root

--- nextLevelWeek/plantmanager/App.tsx
+++ nextLevelWeek/plantmanager/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Welcome } from './source/pages/Welcome';
+import React from 'react'
+import { Welcome } from './source/pages/Welcome'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
+  const [loaded] = useFonts({
+    Jost_400Regular, 
+    Jost_600SemiBold
+  })
+  
+  if (!loaded) {
+    return <AppLoading />
+  }
+
   return (
     <Welcome />
-  );
+  )
 }
-
