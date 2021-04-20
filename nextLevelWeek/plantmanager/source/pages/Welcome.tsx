9e8ed0f884f33e03b3c874d97874bcf943aa348b
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-20 11:37:59 -0300

    adicionando estilos para a tela Welcome

--- nextLevelWeek/plantmanager/source/pages/Welcome.tsx
+++ nextLevelWeek/plantmanager/source/pages/Welcome.tsx
@@ -1,23 +1,67 @@
 import React from 'react'
 
-import { Text, View, Image, TouchableOpacity } from 'react-native'
+import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet } from 'react-native'
 
 import wateringImg from '../assets/watering.png'
+import colors from '../styles/colors'
 
 export function Welcome() {
     return (
-        <View>
-            <Text>
-                Welcome
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'} suas plantas {'\n'} de forma fácil
             </Text>
 
-            <Image source={wateringImg}/>
+            <Image source={wateringImg} style={styles.imageContent}/>
 
-            <TouchableOpacity>
-                <Text>
+            <Text style={styles.caption}>
+                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
+            </Text>
+
+            <TouchableOpacity style={styles.buttonContainer} activeOpacity={0.7}>
+                <Text style={styles.buttonContent}>
                     >
                 </Text>
             </TouchableOpacity>
-        </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingTop: 40,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading
+    },
+    caption: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: colors.heading,
+        paddingHorizontal: 20,
+    },
+    buttonContainer: {
+        backgroundColor: colors.green,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonContent: {
+        color: colors.white,
+        fontSize: 24,
+    },
+    imageContent: {
+        width: 292,
+        height: 284,
+    }
+})
