285152348b6d3021d4b4192e3003f0b3a761ade0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-21 20:01:31 -0300

    refinando tela Welcome

--- nextLevelWeek/plantmanager/source/pages/Welcome.tsx
+++ nextLevelWeek/plantmanager/source/pages/Welcome.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 
-import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet, Dimensions, View } from 'react-native'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors'
 import { Feather } from '@expo/vector-icons'
+import fonts from '../styles/fonts'
 
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'} suas plantas {'\n'} de forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'} suas plantas de {'\n'} forma fácil
+                </Text>
 
-            <Image source={wateringImg} style={styles.imageContent}/>
+                <Image source={wateringImg} style={styles.imageContent}/>
 
-            <Text style={styles.caption}>
-                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+                <Text style={styles.caption}>
+                    Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <TouchableOpacity style={styles.buttonContainer} activeOpacity={0.7}>
-                <Feather size={20} name="chevron-right" style={styles.buttonContent} />
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonContainer} activeOpacity={0.7}>
+                    <Feather size={20} name="chevron-right" style={styles.buttonContent} />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -29,20 +32,28 @@ export function Welcome() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'space-around',
+        paddingHorizontal: 20,
+        paddingTop: 20,
     },
     title: {
         fontSize: 32,
         fontWeight: 'bold',
         textAlign: 'center',
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 34,
     },
     caption: {
         fontSize: 18,
         textAlign: 'center',
         color: colors.heading,
         paddingHorizontal: 20,
+        fontFamily: fonts.text,
     },
     buttonContainer: {
         backgroundColor: colors.green,
