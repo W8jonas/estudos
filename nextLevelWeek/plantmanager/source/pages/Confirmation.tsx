9f53ba7416de6105f1b458cccf597f8238e7f8cd
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-22 12:29:06 -0300

    navegando para PlantSelect

--- nextLevelWeek/plantmanager/source/pages/Confirmation.tsx
+++ nextLevelWeek/plantmanager/source/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core'
 import React from 'react'
 import { Text, SafeAreaView, View, StyleSheet } from 'react-native'
 
@@ -6,6 +7,12 @@ import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 export function Confirmation() {
+    const navigation = useNavigation()
+
+    function onNavigation() {
+        navigation.navigate('PlantSelect')
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.wrapper}>
@@ -22,7 +29,7 @@ export function Confirmation() {
                 </Text>
 
                 <View style={styles.footer}>
-                    <Button title="Começar"/>
+                    <Button title="Começar" onPress={onNavigation}/>
                 </View>
             </View>
         </SafeAreaView>
