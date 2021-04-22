e5f7b40f66c9b276375059605a8fd342fc4287f0
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-21 21:41:14 -0300

    att Button props

--- nextLevelWeek/plantmanager/source/pages/Confirmation.tsx
+++ nextLevelWeek/plantmanager/source/pages/Confirmation.tsx
@@ -22,7 +22,7 @@ export function Confirmation() {
                 </Text>
 
                 <View style={styles.footer}>
-                    <Button />
+                    <Button title="Começar"/>
                 </View>
             </View>
         </SafeAreaView>
