ab002979f1ddad4e77ca9955c472ada20520ffa9
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-21 21:34:21 -0300

    criando navigationContainer

---
+++ nextLevelWeek/plantmanager/source/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+import StackRouts from './stack'
+
+const Routes = () => (
+    <NavigationContainer>
+        <StackRouts />
+    </NavigationContainer>
+)
+
+export default Routes
