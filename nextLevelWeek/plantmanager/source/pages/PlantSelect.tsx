b52d77d5d29d309b8e717fe27ff7d4e94156971f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-23 22:46:30 -0300

    adicionando FlatList para EnvironmentButton

--- nextLevelWeek/plantmanager/source/pages/PlantSelect.tsx
+++ nextLevelWeek/plantmanager/source/pages/PlantSelect.tsx
@@ -1,27 +1,42 @@
 import React from 'react'
-import { Text, SafeAreaView, View, StyleSheet } from 'react-native'
+import { Text, SafeAreaView, View, StyleSheet, FlatList } from 'react-native'
 
 import {Button} from '../components/Button'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
 import { Header } from '../components/Header'
+import { EnvironmentButton } from '../components/EnvironmentButton'
 
 export function PlantSelect() {
     return (
         <SafeAreaView style={styles.container}>
 
-            <Header />
+            <View style={styles.header}>
+                <Header />
 
-            <View style={styles.wrapper}>
-                <Text style={styles.emoji}>
-                    😃
+                <Text style={styles.title}>
+                    Em qual hambiente 
                 </Text>
                 
-                <Text style={styles.title}>
-                    Estamos prontos!
+                <Text style={styles.caption}>
+                    você quer colocar sua planta?
                 </Text>
+            </View>
 
+            <View>
+                <FlatList
+                data={[1, 2, 3, 4, 5, 6, 7 ]}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={( {item} ) => (
+                    <EnvironmentButton title="asas" active/>
+                )}
+                contentContainerStyle={styles.EnvironmentList}
+                />
+            </View>
+
+            <View style={styles.wrapper}>
                 <View style={styles.footer}>
                     <Button title="Começar"/>
                 </View>
@@ -35,9 +50,11 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'space-around',
         backgroundColor: colors.background
+    },
+    header: {
+        paddingHorizontal: 30,
     },
     wrapper: {
         flex: 1,
@@ -46,24 +63,25 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 30
     },
-    emoji: {
-        fontSize: 78,
-        color: colors.heading,
+    EnvironmentList: {
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginLeft: 32,
+        marginVertical: 32,
     },
     title: {
-        fontSize: 22,
-        lineHeight: 38,
-        textAlign: 'center',
+        fontSize: 17,
+        lineHeight: 20,
         color: colors.heading,
         fontFamily: fonts.heading,
         marginTop: 15,
     },
-    text: {
+    caption: {
         fontSize: 17,
-        textAlign: 'center',
-        paddingVertical: 20,
+        lineHeight: 20,
         color: colors.heading,
-        fontFamily: fonts.text
+        fontFamily: fonts.heading,
     },
     footer: {
         width: '100%',
