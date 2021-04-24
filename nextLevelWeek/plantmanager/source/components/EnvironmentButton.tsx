c4415eb68e7e3af34612dc36cd9fd53d19de31e6
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-23 22:46:17 -0300

    refinando estilos do EnvironmentButton

--- nextLevelWeek/plantmanager/source/components/EnvironmentButton.tsx
+++ nextLevelWeek/plantmanager/source/components/EnvironmentButton.tsx
@@ -14,8 +14,11 @@ interface EnvironmentButtonProps extends RectButtonProps {
 
 export function EnvironmentButton({title, active = false, ...props}: EnvironmentButtonProps) {
     return (
-        <RectButton style={styles.container} {...props}>
-        <Text style={styles.text}>
+        <RectButton 
+            style={[styles.container, active && styles.containerActive]} 
+            {...props}
+        >
+        <Text style={[styles.text, active && styles.textActive]}>
             {title}
         </Text>
     </RectButton>
@@ -30,9 +33,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 12,
         backgroundColor: colors.shape,
+        marginRight: 5
+    },
+    containerActive: {
+        backgroundColor: colors.green_light
     },
     text: {
         color: colors.heading,
+        fontFamily: fonts.text
+    },
+    textActive: {
+        color: colors.green_dark,
         fontFamily: fonts.text
     },
 })
