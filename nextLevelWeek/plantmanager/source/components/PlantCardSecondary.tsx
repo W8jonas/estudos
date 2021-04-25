9f07f6d39f3b4a46029c23823b04c38a60910dce
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-25 17:01:09 -0300

    criando PlantCardSecondary

---
+++ nextLevelWeek/plantmanager/source/components/PlantCardSecondary.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import { Text, StyleSheet, View} from 'react-native'
+
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
+import {SvgFromUri} from 'react-native-svg'
+
+interface PlantCardSecondaryProps extends RectButtonProps {
+    data: {
+        name: string,
+        photo: string,
+        hour: string,
+    }
+}
+
+export function PlantCardSecondary({data, ...props}: PlantCardSecondaryProps) {
+    return (
+        <RectButton 
+            style={styles.container} 
+            {...props}
+        >
+            <SvgFromUri uri={data.photo} width={'150%'}/>
+            
+            <Text style={styles.text}>
+                {data.name}
+            </Text>
+
+            <View style={styles.details}>
+                <Text style={styles.timeLabel}>
+                    Regar às
+                </Text>
+                <Text style={styles.time}>
+                    {data.hour}
+                </Text>
+            </View>
+
+        </RectButton>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        paddingHorizontal: 10,
+        paddingVertical: 25,
+        borderRadius: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: colors.shape,
+        marginVertical: 5
+    },
+    text: {
+        flex: 1,
+        marginLeft: 10,
+        fontFamily: fonts.heading,
+    },
+    details: {
+        alignItems: 'flex-end',
+    },
+    timeLabel: {
+        fontSize: 16,
+        fontFamily: fonts.text,
+        color: colors.body_light,
+    },
+    time: {
+        marginTop: 5,
+        fontSize: 16,
+        fontFamily: fonts.heading,
+        color: colors.body_dark
+    }
+})
