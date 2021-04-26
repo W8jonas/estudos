43b44035016c27091015581ca4f22cd8f5925956
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-26 02:49:27 -0300

    criando notificação

--- nextLevelWeek/plantmanager/source/libs/storage.ts
+++ nextLevelWeek/plantmanager/source/libs/storage.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { format } from 'date-fns/esm'
-
+import * as Notifications from 'expo-notifications';
 
 export interface PlantProps {
     id: string,
@@ -19,20 +19,54 @@ export interface PlantProps {
 
 export interface StoragePlantProps {
     [id: string]: {
-        data: PlantProps
+        data: PlantProps,
+        notificationId: string
     }
 }
 
 
 export async function savePlant(plant: PlantProps) : Promise<void> {
-
     try {
+
+        const nexTime = new Date(plant.dateTimeNotification)
+        const now = new Date()
+
+        const { times, repeat_every } = plant.frequency
+        if (repeat_every === 'week') {
+            const interval = Math.trunc(7 / times)
+            nexTime.setDate(now.getDate() + interval)
+        } else {
+            nexTime.setDate(now.getDate() + 1)
+        }
+
+        const seconds = Math.abs(
+            Math.ceil((now.getTime() - nexTime.getTime()) / 1000)
+        )
+
+        const notificationId = await Notifications.scheduleNotificationAsync({
+            content: {
+                title: 'Eiii',
+                body: `Èstá na hora de cuidar da sua ${plant.name}`,
+                sound: true,
+                priority: Notifications.AndroidNotificationPriority.HIGH,
+                data: {
+                    plant
+                },
+            },
+            trigger: {
+                seconds: 60,
+                // seconds: seconds < 60 ? 60 : seconds,
+                repeats: true
+            }
+        })
+
         const data = await AsyncStorage.getItem('@plantmanager:plants')
         const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {}
 
         const newPlant = {
             [plant.id]: {
-                data: plant
+                data: plant,
+                notificationId
             }
         }
 
