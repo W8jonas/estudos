ffff6d02148df37ee43a8f58d4b74f68991b20b2
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-25 22:14:48 -0300

    centralizando Load comp

--- nextLevelWeek/plantmanager/source/components/Load.tsx
+++ nextLevelWeek/plantmanager/source/components/Load.tsx
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
     },
     animation: {
         width: 70,
