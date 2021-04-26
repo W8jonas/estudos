dfbfd5b8ad19b5cd468dd5a9595683b43e94315e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-26 00:13:10 -0300

    adicionando ScrollView tela PlantSave

--- nextLevelWeek/plantmanager/source/pages/PlantSave.tsx
+++ nextLevelWeek/plantmanager/source/pages/PlantSave.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Text, Platform, View, StyleSheet, Alert, Image, TouchableOpacity } from 'react-native'
+import { Text, Platform, View, StyleSheet, Alert, Image, TouchableOpacity, ScrollView } from 'react-native'
 
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
@@ -67,55 +67,60 @@ export function PlantSave() {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.plantInfo}>
-                <SvgFromUri uri={plant.photo} width={150} height={150} />
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.container}
+        >
+            <View style={styles.container}>
+                <View style={styles.plantInfo}>
+                    <SvgFromUri uri={plant.photo} width={150} height={150} />
 
-                <Text style={styles.plantName}>
-                    {plant.name}
-                </Text>
+                    <Text style={styles.plantName}>
+                        {plant.name}
+                    </Text>
 
-                <Text style={styles.plantAbout}>
-                    {plant.about}
-                </Text>
-            </View>
-
-            <View style={styles.controller}>
-                <View style={styles.tipContainer}>
-                    <Image source={waterDrop} style={styles.tipImage} />
-                    <Text style={styles.tipText}>
-                        {plant.water_tips}
+                    <Text style={styles.plantAbout}>
+                        {plant.about}
                     </Text>
                 </View>
 
-                <Text style={styles.alertLabel}>
-                    Escolha o melhor horário para ser lembrado
-                </Text>
+                <View style={styles.controller}>
+                    <View style={styles.tipContainer}>
+                        <Image source={waterDrop} style={styles.tipImage} />
+                        <Text style={styles.tipText}>
+                            {plant.water_tips}
+                        </Text>
+                    </View>
 
-                {showDatePicker && (
-                    <DateTimePicker 
-                        value={selectedDateTime}
-                        mode="time"
-                        display="spinner"
-                        onChange={handleChangeTime}
-                    />
-                )}
+                    <Text style={styles.alertLabel}>
+                        Escolha o melhor horário para ser lembrado
+                    </Text>
 
-                {
-                    Platform.OS === 'android' && (
-                        <TouchableOpacity 
-                            style={styles.androidDateTimePickerButton}
-                            onPress={handleOpenDateTimePickerForAndroid}
-                        >
-                            <Text style={styles.androidDateTimePickerText}>
-                                Mudar {format(selectedDateTime, 'HH:mm')}
-                            </Text>
-                        </TouchableOpacity>
-                )}
+                    {showDatePicker && (
+                        <DateTimePicker 
+                            value={selectedDateTime}
+                            mode="time"
+                            display="spinner"
+                            onChange={handleChangeTime}
+                        />
+                    )}
 
-                <Button title="Cadastrar planta" onPress={handleSave}/>
+                    {
+                        Platform.OS === 'android' && (
+                            <TouchableOpacity 
+                                style={styles.androidDateTimePickerButton}
+                                onPress={handleOpenDateTimePickerForAndroid}
+                            >
+                                <Text style={styles.androidDateTimePickerText}>
+                                    Mudar {format(selectedDateTime, 'HH:mm')}
+                                </Text>
+                            </TouchableOpacity>
+                    )}
+
+                    <Button title="Cadastrar planta" onPress={handleSave}/>
+                </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
