caa0bc4e7926640849b6745d70194b038959e46d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-26 02:53:09 -0300

    removendo notificações da planta que foram excluídas

--- nextLevelWeek/plantmanager/source/libs/storage.ts
+++ nextLevelWeek/plantmanager/source/libs/storage.ts
@@ -109,6 +109,8 @@ export async function deletePlant(id: string) : Promise<void> {
         const data = await AsyncStorage.getItem('@plantmanager:plants')
         const plants = data ? (JSON.parse(data) as StoragePlantProps) : {}
 
+        await Notifications.cancelScheduledNotificationAsync(plants[id].notificationId)
+
         delete plants[id]
 
         await AsyncStorage.setItem('@plantmanager:plants', JSON.stringify(plants))
