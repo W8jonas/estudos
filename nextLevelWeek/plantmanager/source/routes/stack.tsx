d6d214380e456aa54472408fb71617eccfe4754b
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-25 22:14:37 -0300

    criando tab navigator

--- nextLevelWeek/plantmanager/source/routes/stack.tsx
+++ nextLevelWeek/plantmanager/source/routes/stack.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import colors from '../styles/colors'
+
 import { Welcome } from '../pages/Welcome'
 import { Confirmation } from '../pages/Confirmation'
 import { UserIdentification } from '../pages/UserIdentification'
-import { PlantSelect } from '../pages/PlantSelect'
 import { PlantSave } from '../pages/PlantSave'
 import { MyPlants } from '../pages/MyPlants'
+
+import AuthRoutes from './tab'
 
 const StackRouts = createStackNavigator()
 
@@ -22,9 +24,9 @@ const AppRoutes: React.FC = () => (
         <StackRouts.Screen name="UserIdentification" component={UserIdentification} />
         <StackRouts.Screen name="Confirmation" component={Confirmation} />
 
-        <StackRouts.Screen name="PlantSelect" component={PlantSelect} />
+        <StackRouts.Screen name="PlantSelect" component={AuthRoutes} />
         <StackRouts.Screen name="PlantSave" component={PlantSave} />
-        <StackRouts.Screen name="MyPlants" component={MyPlants} />
+        <StackRouts.Screen name="MyPlants" component={AuthRoutes} />
 
     </StackRouts.Navigator>
 )
