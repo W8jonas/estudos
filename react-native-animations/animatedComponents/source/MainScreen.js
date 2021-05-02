79756a21e5a8cc047f3e7b100ec5e00a98dba9dc
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-02 19:14:50 -0300

    adicionando as duas novas rotas

--- react-native-animations/animatedComponents/source/MainScreen.js
+++ react-native-animations/animatedComponents/source/MainScreen.js
@@ -3,11 +3,22 @@ import {
 	View, Text, TouchableOpacity, StyleSheet,
 } from 'react-native'
 
-export function MainScreen() {
+export function MainScreen({ navigation }) {
 	return (
 		<View style={styles.container}>
-			<TouchableOpacity style={styles.buttonContainer}>
-				<Text style={styles.buttonContent}>MainScreen</Text>
+
+			<TouchableOpacity
+				style={styles.buttonContainer}
+				onPress={() => navigation.navigate('DownloadButton')}
+			>
+				<Text style={styles.buttonContent}>DownloadButton</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+				style={styles.buttonContainer}
+				onPress={() => navigation.navigate('ProgressBar')}
+			>
+				<Text style={styles.buttonContent}>ProgressBar</Text>
 			</TouchableOpacity>
 
 		</View>
@@ -17,14 +28,17 @@ export function MainScreen() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#FFF',
 		padding: 20,
+		flexWrap: 'wrap',
+		backgroundColor: '#FFF',
 	},
 	buttonContainer: {
-		width: 120,
+		width: 140,
 		height: 50,
 		justifyContent: 'center',
 		alignItems: 'center',
+		marginVertical: 20,
+		marginLeft: 20,
 		borderRadius: 16,
 		backgroundColor: '#E1E2E7',
 	},
