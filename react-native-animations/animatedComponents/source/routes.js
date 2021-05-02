bf794e1bc03090e3e885f787b77da289f3dde1fc
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-02 19:15:00 -0300

    criando todas as três rotas

--- react-native-animations/animatedComponents/source/routes.js
+++ react-native-animations/animatedComponents/source/routes.js
@@ -3,7 +3,10 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import DownloadButton from './screen/downloadButton/index'
+// Screens
+import { MainScreen } from './MainScreen'
+import { DownloadButton } from './screen/downloadButton/index'
+import { ProgressBar } from './screen/progressBar/index'
 
 const Stack = createStackNavigator()
 
@@ -16,11 +19,22 @@ function App() {
 		<NavigationContainer>
 			<Stack.Navigator>
 				<Stack.Screen
+					name="MainScreen"
+					component={MainScreen}
+					options={defaultConfigForScreens}
+				/>
+
+				<Stack.Screen
 					name="DownloadButton"
 					component={DownloadButton}
 					options={defaultConfigForScreens}
 				/>
 
+				<Stack.Screen
+					name="ProgressBar"
+					component={ProgressBar}
+					options={defaultConfigForScreens}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
