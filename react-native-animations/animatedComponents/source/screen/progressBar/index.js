7dca3285e25f59f4e368daa0324e39862a08719f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-02 19:12:48 -0300

    removendo export default do ProgressBar

---
+++ react-native-animations/animatedComponents/source/screen/progressBar/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+export function ProgressBar() {
+	return (
+		<View>
+			<Text>ProgressBar</Text>
+		</View>
+	)
+}
