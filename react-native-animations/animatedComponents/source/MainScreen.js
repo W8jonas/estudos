2c60930757415084fb0e599a00dbe8e0ad65d3bb
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-02 18:53:39 -0300

    criando base MainScreen

---
+++ react-native-animations/animatedComponents/source/MainScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+function MainScreen() {
+	return (
+		<View>
+			<TouchableOpacity>
+				<Text>MainScreen</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+export default MainScreen
