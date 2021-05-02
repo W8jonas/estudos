8cd2f7b84a78ad53d4c8fba669c6cb25d6be62a8
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-02 19:02:41 -0300

    adicionando estilos

--- react-native-animations/animatedComponents/source/MainScreen.js
+++ react-native-animations/animatedComponents/source/MainScreen.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import {
+	View, Text, TouchableOpacity, StyleSheet,
+} from 'react-native'
 
-function MainScreen() {
+export function MainScreen() {
 	return (
-		<View>
-			<TouchableOpacity>
-				<Text>MainScreen</Text>
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.buttonContainer}>
+				<Text style={styles.buttonContent}>MainScreen</Text>
 			</TouchableOpacity>
+
 		</View>
 	)
 }
 
-export default MainScreen
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#FFF',
+		padding: 20,
+	},
+	buttonContainer: {
+		width: 120,
+		height: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 16,
+		backgroundColor: '#E1E2E7',
+	},
+	buttonContent: {
+		fontSize: 16,
+		color: '#1E1E1F',
+	},
+})
