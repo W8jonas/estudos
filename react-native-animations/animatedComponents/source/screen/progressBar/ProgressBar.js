4f76525ec3bbaee88a1adaa9788701b7577c5802
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-05 16:00:57 -0300

    criando PregressBar estática

--- react-native-animations/animatedComponents/source/screen/progressBar/ProgressBar.js
+++ react-native-animations/animatedComponents/source/screen/progressBar/ProgressBar.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 export function ProgressBar() {
 	return (
-		<View>
-			<Text>ProgressBar</Text>
+		<View style={styles.container}>
+			<View
+				style={styles.progressBar}
+			/>
+
+			<View
+				style={styles.fluidBar}
+			/>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	progressBar: {
+		height: 12,
+		width: 100,
+		borderRadius: 12,
+		backgroundColor: '#F60',
+	},
+	fluidBar: {
+		position: 'absolute',
+		height: 12,
+		width: 50,
+		borderRadius: 12,
+		backgroundColor: '#06F',
+	},
+})
