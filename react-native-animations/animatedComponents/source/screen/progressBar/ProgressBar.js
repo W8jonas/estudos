9e4b7ba6ed16e4f7c4b0b68f7080d961816d012f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-05 16:15:42 -0300

    adicionando variáveis de configuraçoes

--- react-native-animations/animatedComponents/source/screen/progressBar/ProgressBar.js
+++ react-native-animations/animatedComponents/source/screen/progressBar/ProgressBar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
+const BAR_PROGRESS_WIDTH = 220
+const INITIAL_PROGRESS = 10
+const GAIN = 60
+const TOTAL_PROGRESS = INITIAL_PROGRESS + GAIN
+
 export function ProgressBar() {
 	return (
 		<View style={styles.container}>
@@ -9,7 +14,7 @@ export function ProgressBar() {
 			/>
 
 			<View
-				style={styles.fluidBar}
+				style={[styles.fluidBar, { width: ((TOTAL_PROGRESS * BAR_PROGRESS_WIDTH) / 100) }]}
 			/>
 		</View>
 	)
@@ -21,14 +26,14 @@ const styles = StyleSheet.create({
 	},
 	progressBar: {
 		height: 12,
-		width: 100,
+		width: BAR_PROGRESS_WIDTH,
 		borderRadius: 12,
 		backgroundColor: '#F60',
 	},
 	fluidBar: {
 		position: 'absolute',
 		height: 12,
-		width: 50,
+		width: BAR_PROGRESS_WIDTH / 2,
 		borderRadius: 12,
 		backgroundColor: '#06F',
 	},
