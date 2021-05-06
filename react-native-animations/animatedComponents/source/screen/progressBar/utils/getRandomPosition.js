8769982ab6cd2f80a7a2b6650378a8b99c174ac7
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-06 16:57:07 -0300

    gerando numeros positivos e negativos

--- react-native-animations/animatedComponents/source/screen/progressBar/utils/getRandomPosition.js
+++ react-native-animations/animatedComponents/source/screen/progressBar/utils/getRandomPosition.js
@@ -2,8 +2,9 @@ export function getRandomPosition({ x: _maxValueX, y: _maxValueY }) {
 	const maxValueX = _maxValueX
 	const maxValueY = _maxValueY
 
-	const valueX = Math.floor(Math.random() * maxValueX) + 1
-	const valueY = Math.floor(Math.random() * maxValueY) + 1
+	// random positive int
+	const valueX = (Math.floor(Math.random() * maxValueX) + 1) * (Math.random() >= 0.5 ? 1 : -1)
+	const valueY = (Math.floor(Math.random() * maxValueY) + 1) * (Math.random() >= 0.5 ? 1 : -1)
 
 	return { x: valueX, y: valueY }
 }
