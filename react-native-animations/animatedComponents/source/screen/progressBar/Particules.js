e92fd77d3f5f0b32fa56326a9553b214046f666b
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-05 23:10:12 -0300

    Colocando posição inicial da particula no início do componente

--- react-native-animations/animatedComponents/source/screen/progressBar/Particules.js
+++ react-native-animations/animatedComponents/source/screen/progressBar/Particules.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 export function Particules() {
 	return (
-		<View>
-			<Text>Particules</Text>
-		</View>
+		<View style={styles.container} />
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		position: 'absolute',
+		top: 0,
+		height: 12,
+		width: 12,
+		borderRadius: 12,
+		backgroundColor: '#F5f9',
+	},
+})
