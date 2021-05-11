6ae05a3b2e6610a7243ac33aabb41a9689c55b2f
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-11 01:23:30 -0300

    adicionando animação de diminuição da escala das particulas

--- react-native-animations/animatedComponents/source/screen/progressBar/Particle.js
+++ react-native-animations/animatedComponents/source/screen/progressBar/Particle.js
@@ -5,22 +5,39 @@ import { StyleSheet, Animated, Easing } from 'react-native'
 import { getRandomPosition } from './utils/getRandomPosition'
 
 export function Particle({ particle }) {
+	const [scale] = useState(new Animated.Value(1))
+
 	useEffect(() => {
 		function animation(_particle) {
-			Animated.timing(_particle.positionXY, {
-				toValue: getRandomPosition({ x: 40, y: 40 }),
-				duration: 1200,
-				useNativeDriver: false,
-				easing: Easing.linear,
-			}).start()
+			Animated.parallel([
+				Animated.timing(_particle.positionXY, {
+					toValue: getRandomPosition({ x: 40, y: 40 }),
+					duration: 1200,
+					useNativeDriver: false,
+					easing: Easing.linear,
+				}),
+				Animated.timing(scale, {
+					toValue: 0,
+					duration: 1200,
+					useNativeDriver: false,
+					easing: Easing.linear,
+				}),
+			]).start()
 		}
 
 		animation(particle)
-	}, [particle])
+	}, [particle, scale])
 
 	return (
 		<Animated.View
-			style={[styles.container, particle.positionXY.getLayout()]}
+			style={[
+				styles.container,
+				particle.positionXY.getLayout(),
+				{
+					opacity: particle.opacity,
+					transform: [{ scale }],
+				},
+			]}
 		/>
 	)
 }
