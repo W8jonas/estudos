b92ccbef0c758fb054c9ad6cb5a61a8c0240ce06
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-14 22:15:52 -0300

    colocando contador e limitador de partículas

--- react-native-animations/animatedComponents/source/screen/progressBar/Particules.js
+++ react-native-animations/animatedComponents/source/screen/progressBar/Particules.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Animated } from 'react-native'
 
 import { Particle } from './Particle'
@@ -11,9 +11,19 @@ export function Particules({ amount, initialPosition }) {
 		opacity: 1,
 	}))
 
+	const [totalParticlesToShow, setTotalParticlesToShow] = useState(1)
+
+	useEffect(() => {
+		setInterval(() => {
+			setTotalParticlesToShow((state) => state + 1)
+		}, 1000)
+	}, [])
+
 	return (
 		<>
-			{particlesArray.map((particle) => <Particle key={particle.id} particle={particle} />)}
+			{particlesArray.map((particle, index) => (
+				totalParticlesToShow >= index ? <Particle key={particle.id} particle={particle} /> : null
+			))}
 		</>
 	)
 }
