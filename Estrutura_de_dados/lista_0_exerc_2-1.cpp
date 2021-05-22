86cc9c926d7d4d8bd301c5bf9ed893268458c351
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-21 21:35:36 -0300

    terminando 1ª parte exercício

--- Estrutura_de_dados/lista_0_exerc_2-1.cpp
+++ Estrutura_de_dados/lista_0_exerc_2-1.cpp
@@ -5,14 +5,48 @@ using namespace std;
 int main()
 {
 
-    float salary = 0;
     float allSalary = 0;
     float bestSalary = 0;
 
-    int totalOfChields = 0;
+    int allChildren = 0;
     int counterSalary = 0;
     int counterPeople = 0;
+    
+    cout << "\n";
+    while (1) {
+        float salary = 0;
+        int children = 0;
+        
+        cout << "\n";
+        cout << "Digite o seu salario: ";
+        cin >> salary;
 
+        if(salary < 0) {
+            break;
+        }
+        counterPeople ++;
+
+        cout << "Digite quantos filhos voce tem: ";
+        cin >> children;
+        
+        if (salary >= 100) {
+            counterSalary++;
+        }
+        
+        if (salary > bestSalary) {
+            bestSalary = salary;
+        }
+
+        allSalary += salary;
+        allChildren += children;
+    }
+
+    cout << "\n";
+    cout << "Media de salario da populacao: " << allSalary / counterPeople << endl;
+    cout << "Media de numero de filhos: " << allChildren / counterPeople << endl;
+    cout << "Maior salario: " << bestSalary << endl;
+    cout << "Percentual de pessoas com salario maior que R$100,00: " << (counterSalary/counterPeople)*100 << "%" << endl;
+    cout << "\n\n";
 
     return 0;
 }
