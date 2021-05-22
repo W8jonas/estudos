eabf6694715f00873e68ff63460de2027fd0d698
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-05-21 21:10:13 -0300

    iniciando exercício 2 - 1

---
+++ Estrutura_de_dados/lista_0_exerc_2-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    float salary = 0;
+    float allSalary = 0;
+    float bestSalary = 0;
+
+    int totalOfChields = 0;
+    int counterSalary = 0;
+    int counterPeople = 0;
+
+
+    return 0;
+}
