a4d7af0e975b9cafc4e4f33cfb167398d71d3ff8
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-18 23:50:27 -0300

    iniciando com array.cpp

---
+++ Estrutura_de_dados/ClassArray/Array.cpp
@@ -0,0 +1,22 @@
+#include "Array.h"
+#include <math.h>
+
+
+Array::Array(int tam)
+{
+
+}
+
+Array::~Array() {}
+
+void Array::set(int indice, float valor) {
+    
+}
+
+float Array::get(int indice) {
+    return 1;
+}
+
+bool Array::verifica(int indice){
+
+}
