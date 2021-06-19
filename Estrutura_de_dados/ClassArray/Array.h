e9a27f90a85fd40c5f40551fc6f1c90e174c652d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-18 23:50:18 -0300

    corrigindo funcao construtora

--- Estrutura_de_dados/ClassArray/Array.h
+++ Estrutura_de_dados/ClassArray/Array.h
@@ -4,7 +4,7 @@ class Array {
         float *vet;
         bool verifica(int indice);
     public:
-        Array(int _numLados, float _tamanhoLado);
+        Array(int tam);
         ~Array();
         
         void set(int indice, float valor);
