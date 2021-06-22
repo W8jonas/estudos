3b288ca52584b86ad5a6b48171153172961cdac2
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-21 22:08:21 -0300

    programa de exemplo para classe Array

--- Estrutura_de_dados/ClassArray/exemplo.cpp
+++ Estrutura_de_dados/ClassArray/exemplo.cpp
@@ -5,6 +5,19 @@ using namespace std;
 
 
 int main() {
+    Array vetor(10);
+
+    for (int i = 0; i < 10; i++) {
+        float number;
+        cin >> number;
+        vetor.set(i,  number);
+    }
+    
+
+    for (int i = 0; i < 10; i++) {
+        cout << vetor.set(i) << ", ";
+    }
+    cout << endl;
 
     return 0;
 }
