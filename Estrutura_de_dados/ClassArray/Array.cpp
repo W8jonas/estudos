02f31758e184127d79e7817dc053db87054f183d
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-21 21:14:18 -0300

    criando get class Array

--- Estrutura_de_dados/ClassArray/Array.cpp
+++ Estrutura_de_dados/ClassArray/Array.cpp
@@ -21,7 +21,10 @@ void Array::set(int indice, float valor) {
 }
 
 float Array::get(int indice) {
-    return 1;
+    if (verifica(indice)) {
+        return vet[indice];
+    }
+    return -1;
 }
 
 bool Array::verifica(int indice){
