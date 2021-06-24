fa76fa53934205f83b042ed5f31098d83c03fe26
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-24 16:44:17 -0300

    iniciando construtor

--- Estrutura_de_dados/ClassMatrizLinear/MatrizLin.cpp
+++ Estrutura_de_dados/ClassMatrizLinear/MatrizLin.cpp
@@ -1,12 +1,17 @@
 #include "MatrizLin.h"
 #include <math.h>
 
-MatrizLin::MatrizLin(int tam) {
-    
+MatrizLin::MatrizLin(int _line, int _column) {
+
+    length = _line * _column;
+
+    vet = new float [length];
+    numLine = _line;
+    numColumn = _column;
 }
 
 MatrizLin::~MatrizLin() {
-
+    delete [] vet;
 }
 
 void MatrizLin::set(int line, int column, float valor) {
