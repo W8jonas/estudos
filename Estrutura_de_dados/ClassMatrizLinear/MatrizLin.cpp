5fb9a828bf67e7425551eb24f4ac1bc4a18034ad
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-24 13:46:53 -0300

    criando MatrizLin.cpp

---
+++ Estrutura_de_dados/ClassMatrizLinear/MatrizLin.cpp
@@ -0,0 +1,22 @@
+#include "MatrizLin.h"
+#include <math.h>
+
+MatrizLin::MatrizLin(int tam) {
+    
+}
+
+MatrizLin::~MatrizLin() {
+
+}
+
+void MatrizLin::set(int line, int column, float valor) {
+
+}
+
+float MatrizLin::get(int line, int column) {
+
+}
+
+bool MatrizLin::detInd(int indice){
+
+}
