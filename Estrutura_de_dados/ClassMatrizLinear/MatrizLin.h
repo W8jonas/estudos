a0a2051bf6812ca53ebe0496b4c45836aec5cc75
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-24 16:42:16 -0300

    colocado metodo getLength no MatrizLin.h

--- Estrutura_de_dados/ClassMatrizLinear/MatrizLin.h
+++ Estrutura_de_dados/ClassMatrizLinear/MatrizLin.h
@@ -4,11 +4,13 @@ class MatrizLin {
         int numColumn;
         float *vet;
         bool detInd(int index);
+        int length;
 
     public:
-        MatrizLin(int tam);
+        MatrizLin(int line, int column);
         ~MatrizLin();
         
         float get(int line, int column);
         void set(int line, int column, float val);
+        int getLength();
 };
