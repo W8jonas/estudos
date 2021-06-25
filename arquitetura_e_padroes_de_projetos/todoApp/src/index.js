5fd4fc9997015cacb38819e5dcd37435e8059eaf
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-24 23:49:14 -0300

    iniciando configurações de notifications

--- arquitetura_e_padroes_de_projetos/todoApp/src/index.js
+++ arquitetura_e_padroes_de_projetos/todoApp/src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { View } from 'react-native'
 
 import BottomTabNavigator from './routes'
+import { pushNotificationCreateChannel, pushNotificationConfigure } from './services/notifications'
+
+pushNotificationConfigure()
+pushNotificationCreateChannel()
 
 const App = () => (
 	<View style={{ flex: 1, backgroundColor: '#FFF' }}>
