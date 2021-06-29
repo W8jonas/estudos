19f8e0bf3706f8346a39304a1c32d1c6e423a25b
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-29 01:12:53 -0300

    criando contexto e signInWithGoogle

--- nextLevelWeek/letmeask/src/App.tsx
+++ nextLevelWeek/letmeask/src/App.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import { Route, BrowserRouter} from 'react-router-dom'
 
+import { createContext } from 'react'
+import {auth, firebase} from './services/firebase'
+
+type UserTypes = {
+  id: string,
+  name: string,
+  avatar: string,
+}
+
+type AuthContextType = {
+  user: UserTypes | undefined,
+  signInWithGoogle: () => void
+}
+
+export const AuthContext = createContext({} as AuthContextType )
+
 function App() {
+
+  const [user, setUser] = useState<UserTypes>()  
+
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+      auth.signInWithPopup(provider).then(result => {
+          if (result.user) {
+            const {displayName, photoURL, uid} = result.user
+
+            if (!displayName || !photoURL) {
+              throw new Error('Missign information')
+            }
+
+            setUser({
+              id: uid,
+              name: displayName,
+              avatar: photoURL
+            })
+          }
+      })
+  }
+
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContext.Provider value={{user, signInWithGoogle}}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContext.Provider>
     </BrowserRouter>
   )
 }
