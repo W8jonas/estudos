3d391482866ad637bfde0894659450241adaca58
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-29 00:34:32 -0300

    add navegação

--- nextLevelWeek/letmeask/src/App.tsx
+++ nextLevelWeek/letmeask/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { Route, BrowserRouter} from 'react-router-dom'
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+  )
 }
 
 export default App;
