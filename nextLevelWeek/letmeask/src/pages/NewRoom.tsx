21595cd712171316ea122569675d2903b1d6284e
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-06-29 00:16:00 -0300

    criando NewRoom

---
+++ nextLevelWeek/letmeask/src/pages/NewRoom.tsx
@@ -0,0 +1,47 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img 
+                    src={illustrationImg} 
+                    alt="Ilustração simbolizando perguntas e respostas"
+                />
+                <strong>Crie salas de Q&amp;A ao-vivo </strong>
+                <p>Tire as duvidas da sua audiência em tempo-real</p>
+            </aside>
+
+            <main>
+                <div className="main-content">
+                    <img 
+                        src={logoImg} 
+                        alt="Letmeask"
+                    />
+
+                    <h2>Criar uma nova sala</h2>
+
+                    <form >
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                        />
+                        <Button type='submit'>
+                            Criar sala
+                        </Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existente? <a href='#'>Clique aqui</a>
+                    </p>
+                    
+                </div>
+            </main>
+        </div>
+    )
+}
+
