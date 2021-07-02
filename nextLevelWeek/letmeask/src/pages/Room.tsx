4f7829be77ace8e827a25e079d83d2baea69c472
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-02 00:26:02 -0300

    criando estrutura HTML Room

--- nextLevelWeek/letmeask/src/pages/Room.tsx
+++ nextLevelWeek/letmeask/src/pages/Room.tsx
@@ -1,6 +1,41 @@
+import logoImg from '../assets/images/logo.svg'
+
+import {Button} from '../components/Button'
+
 export function Room() {
     
     return (
-        <h1>Uma sala ein!?!? </h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetMeAsk" />
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>
+                        Sala React
+                    </h1>
+                    <span>
+                        4 perguntas
+                    </span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="O que você quer perguntar? "
+                    />
+
+                    <div className="form-footer">
+                        <span>
+                            Para enviar uma pergunta, <button>faça seu login.</button>
+                        </span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
