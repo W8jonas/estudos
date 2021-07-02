b4849d0e1de1bcdfaba21f78d6b8763cea0188ab
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-01 23:53:58 -0300

    colocando Switch nas rotas

--- nextLevelWeek/letmeask/src/App.tsx
+++ nextLevelWeek/letmeask/src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
-import { Route, BrowserRouter} from 'react-router-dom'
+import { Route, BrowserRouter, Switch} from 'react-router-dom'
 
 import { AuthContextProvider } from './contexts/AuthContext'
 import { Room } from './pages/Room';
@@ -11,9 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" exact component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   )
