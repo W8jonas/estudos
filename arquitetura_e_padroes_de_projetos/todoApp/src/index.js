32ad260af2f308cfe380478ba39c1528e8374032
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-07-11 02:09:13 -0300

    iniciando com ErrorBoundary no index da aplicação

--- arquitetura_e_padroes_de_projetos/todoApp/src/index.js
+++ arquitetura_e_padroes_de_projetos/todoApp/src/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { View } from 'react-native'
 
+import { func } from 'prop-types'
+import ErrorBoundary from 'react-native-error-boundary'
+
 import BottomTabNavigator from './routes'
 import { pushNotificationCreateChannel, pushNotificationConfigure } from './services/notifications'
 
 pushNotificationConfigure()
 pushNotificationCreateChannel()
 
-const App = () => (
-	<View style={{ flex: 1, backgroundColor: '#FFF' }}>
-		<BottomTabNavigator />
-	</View>
-)
+function errorHandler(error, stackTrace) {
+	console.log('Error: ', stackTrace)
+}
+
+function App() {
+	return (
+
+		<View style={{ flex: 1, backgroundColor: '#FFF' }}>
+			<ErrorBoundary onError={errorHandler}>
+				<BottomTabNavigator />
+			</ErrorBoundary>
+		</View>
+	)
+}
 
 export default App
