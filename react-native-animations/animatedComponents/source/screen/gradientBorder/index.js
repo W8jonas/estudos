a88ea503942aeee9543766c8e5be85908b902993
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-10-11 20:47:17 -0300

    add GradientBorder

---
+++ react-native-animations/animatedComponents/source/screen/gradientBorder/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+export function GradientBorder() {
+	return (
+		<View>
+			<Text>GradientBorder</Text>
+		</View>
+	)
+}
