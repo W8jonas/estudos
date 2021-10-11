26806059ef341bafeab642be83704affc3e9df64
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-10-11 20:47:25 -0300

    add LightShimmerEffect

---
+++ react-native-animations/animatedComponents/source/screen/LightShimmerEffect/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+// Modules
+
+// Assets
+
+// Functions
+
+// Components
+
+export function LightShimmerEffect() {
+	return (
+		<View>
+			<Text>LightShimmerEffect</Text>
+		</View>
+	)
+}
