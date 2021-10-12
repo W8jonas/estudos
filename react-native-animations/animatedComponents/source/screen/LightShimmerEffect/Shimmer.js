a39b814b2548640555ac55c71df5626cc92558d8
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-10-11 21:56:56 -0300

    fazemdp Shimmer

---
+++ react-native-animations/animatedComponents/source/screen/LightShimmerEffect/Shimmer.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import {
+	Dimensions, StyleSheet, View, Animated, Easing,
+} from 'react-native'
+
+import LinearGradient from 'react-native-linear-gradient'
+
+const styles = StyleSheet.create({
+	shimmer: {
+		overflow: 'hidden',
+		borderRadius: 20,
+		backgroundColor: '#0000',
+	},
+})
+
+const SCREEN_WIDTH = Dimensions.get('screen').width
+
+export function Shimmer({ width, height }) {
+	const START = -1
+	const END = 1
+	const DURATION = 2000
+	const COLORS = ['#0000', '#fff', '#0000']
+	const LOCATIONS = [0.3, 0.5, 0.7]
+	const ANIMATION = new Animated.Value(START)
+
+	useEffect(() => {
+		const runAnimation = () => {
+			ANIMATION.setValue(START)
+			Animated.timing(ANIMATION, {
+				toValue: END,
+				duration: DURATION,
+				easing: Easing.linear(),
+				useNativeDriver: true,
+			}).start(runAnimation)
+		}
+
+		runAnimation()
+	}, [])
+
+	const linear = ANIMATION.interpolate({
+		inputRange: [START, END],
+		outputRange: [-SCREEN_WIDTH, SCREEN_WIDTH],
+	})
+
+	return (
+		<View
+			style={[styles.shimmer, {
+				width, height, position: 'absolute', zIndex: 10,
+			}]}
+		>
+			<Animated.View
+				style={{
+					flex: 1,
+					transform: [{ translateX: linear }],
+				}}
+			>
+				<LinearGradient
+					style={{ flex: 1, width: SCREEN_WIDTH }}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 1, y: 0 }}
+					locations={LOCATIONS}
+					colors={COLORS}
+				/>
+			</Animated.View>
+		</View>
+	)
+}
