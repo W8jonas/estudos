1952477f037e70dc26e2a4696dd63fb4113db79f
W8jonas <jonashenrique989@gmail.com>
2024-02-08 19:50:32 -0300

    Adiciona votos no redis

--- nextLevelWeek/expert/src/http/routes/vote-on-poll.ts
+++ nextLevelWeek/expert/src/http/routes/vote-on-poll.ts
@@ -4,6 +4,7 @@ import {z} from 'zod'
 import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'crypto'
+import { redis } from '../../lib/redis'
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -39,6 +40,8 @@ export async function voteOnPoll(app: FastifyInstance) {
                         id: userPreviousVoteOnPoll.id
                     }
                 })
+                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: 'Are you kidding me?'})
             }
@@ -62,6 +65,7 @@ export async function voteOnPoll(app: FastifyInstance) {
             }
         })
 
+        await redis.zincrby(pollId, 1, pollOptionId)
 
         return reply.status(200).send({ sessionId })
     } )
