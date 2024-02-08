19f39318b6ee40dd68035444b9e5ede4ddb0d45c
W8jonas <jonashenrique989@gmail.com>
2024-02-08 00:57:53 -0300

    cria get poll

---
+++ nextLevelWeek/expert/src/http/routes/get-poll.ts
@@ -0,0 +1,34 @@
+
+import {z} from 'zod'
+
+import { prisma } from '../../lib/prisma'
+import { FastifyInstance } from 'fastify'
+
+export async function getPoll(app: FastifyInstance) {
+    app.get('/polls/:id', async (request, reply) => {
+    
+        const getPollParams = z.object({
+            id: z.string().uuid(),
+        })
+    
+        const { id } = getPollParams.parse(request.params)
+    
+        const poll = await prisma.poll.findUnique({
+            where: {
+                id
+            },
+            include: {
+                options: {
+                    select: {
+                        title: true
+                    }
+                }
+            }
+        })
+    
+        return reply.status(200).send({ poll })
+    } )
+
+   
+}
+
--- nextLevelWeek/expert/src/http/server.ts
+++ nextLevelWeek/expert/src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({port: 3333})
     .then(() => {console.log('listening on port 3333')})
