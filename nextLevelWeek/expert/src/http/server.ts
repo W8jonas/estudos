f2e7fab619c2e1c5541f0a660d34cd9c98bb7136
W8jonas <jonashenrique989@gmail.com>
2024-02-08 01:37:33 -0300

    add cookies e vote

--- nextLevelWeek/expert/src/http/server.ts
+++ nextLevelWeek/expert/src/http/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
 const app = fastify()
 
+app.register(cookie, {
+    secret: "polls-app-nlw-secret",
+    hook: "onRequest"
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3333})
     .then(() => {console.log('listening on port 3333')})
