587c7a9a40b5ae146e041d3eaa58f4d7cf7063f4
W8jonas <jonashenrique989@gmail.com>
2024-02-08 21:48:06 -0300

    registra pollResult

--- nextLevelWeek/expert/src/http/server.ts
+++ nextLevelWeek/expert/src/http/server.ts
@@ -1,8 +1,12 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
-import cookie from "@fastify/cookie";
+import { pollResult } from "../websocket/poll-results";
+
 
 const app = fastify()
 
@@ -10,10 +14,12 @@ app.register(cookie, {
     secret: "polls-app-nlw-secret",
     hook: "onRequest"
 })
-
+app.register(websocket)
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResult)
+
 
 app.listen({port: 3333})
     .then(() => {console.log('listening on port 3333')})
