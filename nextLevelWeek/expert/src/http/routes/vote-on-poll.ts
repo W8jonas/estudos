dfe503d33b56cd4f9f6c38023068a53625aa156c
W8jonas <jonashenrique989@gmail.com>
2024-02-08 21:48:52 -0300

    faz a publicação da mudança no canal correto

--- nextLevelWeek/expert/src/http/routes/vote-on-poll.ts
+++ nextLevelWeek/expert/src/http/routes/vote-on-poll.ts
@@ -5,6 +5,7 @@ import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'crypto'
 import { redis } from '../../lib/redis'
+import { voting } from '../../utils/voting-pub-sub'
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -40,7 +41,12 @@ export async function voteOnPoll(app: FastifyInstance) {
                         id: userPreviousVoteOnPoll.id
                     }
                 })
-                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId,
+                    votes: Number(votes),
+                })
 
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: 'Are you kidding me?'})
@@ -65,7 +71,12 @@ export async function voteOnPoll(app: FastifyInstance) {
             }
         })
 
-        await redis.zincrby(pollId, 1, pollOptionId)
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
+        })
 
         return reply.status(200).send({ sessionId })
     } )
