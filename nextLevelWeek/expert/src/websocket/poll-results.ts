6beeba333c491262bc4698c2def66f80ed7c65b0
W8jonas <jonashenrique989@gmail.com>
2024-02-08 21:47:21 -0300

    cria pollResult websocket

---
+++ nextLevelWeek/expert/src/websocket/poll-results.ts
@@ -0,0 +1,19 @@
+
+import { FastifyInstance } from "fastify"
+import { voting } from "../utils/voting-pub-sub"
+import z from "zod"
+
+export async function pollResult(app: FastifyInstance) {
+    app.get('/polls/:pollId/results', { websocket: true}, (connection, request) => {
+
+        const getPollParams = z.object({
+            pollId: z.string().uuid(),
+        })
+
+        const {pollId} = getPollParams.parse(request.params)
+
+        voting.subscribe(pollId, (message) => {
+            connection.socket.send(JSON.stringify(message))
+        })
+    })
+}
